b83df6a0fb79dd8714b68d6e144a709ceda6e357
Logan Stanford <loganstanford53@gmail.com>
2023-08-04 13:24:34 -0400

    add battle tag and tdb to bracket

--- bracket.py
+++ bracket.py
@@ -1,3 +1,4 @@
+import copy
 
 async def get_match_size(num_users_in_round):
     
@@ -7,31 +8,37 @@ async def get_match_size(num_users_in_round):
 
     return int(round_size / 2)
 
-async def add_user_to_match(user, match):
+async def add_user_to_match(user, match, users):
 
     user_index = 0
     if not match[0]['is_bye']:
         user_index = 1
 
+    user_obj = users.find_one({'discord_id': user})
+
     entry = {
         'user': user,
-        'is_bye': False
+        'username': user_obj['battle_tag'].split('#')[0],
+        'is_bye': False,
+        'is_tdb': True
     }
     match[user_index] = entry
 
-async def make_matches_from_users(users_in_round):
+async def make_matches_from_users(users_in_round, db):
     
+    users = db['users']
+
     match_size = await get_match_size(len(users_in_round))
     print(match_size)
 
     matches = []
 
     for i in range(0, match_size):
-        matches.append([{"is_bye": True}, {"is_bye": True}])
+        matches.append([{"is_bye": True, "is_tbd": False}, {"is_bye": True, "is_tbd": False}])
     
     match_index = 0
     for user in users_in_round:
-        await add_user_to_match(user, matches[match_index])
+        await add_user_to_match(user, matches[match_index], users)
 
         match_index += 1
         if match_index == len(matches):
@@ -39,11 +46,27 @@ async def make_matches_from_users(users_in_round):
 
     return matches
 
+async def gen_tbd_round(length_last_round):
+
+    this_round_length = int(length_last_round/2)
+
+    matches = []
+
+    for i in range(0, this_round_length):
+        matches.append([{'is_bye': False, 'is_tdb': True}, {'is_bye': False, 'is_tdb': True}])
+
 async def make_bracket_from_users(all_users):
 
     round1_matches = await make_matches_from_users(all_users)
 
     rounds = [round1_matches]
+
+    len_matches = len(round1_matches)
+    while len_matches != 1:
+        next_round = gen_tbd_round(len_matches)
+        rounds.append(copy.deepcopy(next_round))
+
+        len_matches = len(next_round)
 
     return rounds
     
