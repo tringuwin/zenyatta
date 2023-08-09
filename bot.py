752cec9cee41fb1ad110eda6c2e6c6adfc1c5a26
Logan Stanford <loganstanford53@gmail.com>
2023-08-09 09:50:32 -0400

    fix event command

--- bot.py
+++ bot.py
@@ -150,8 +150,6 @@ def run_discord_bot(mongo_client, db):
             event_list = get_all_events(db)
             found = False
             none_string = "It looks like there's no events right now... Check back soon!"
-            await message.channel.send(none_string)
-            return
 
             final_string = ""
 
