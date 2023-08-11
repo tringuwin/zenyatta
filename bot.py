1cd3f5074c2cba743671662f47335251d26175b7
Logan Stanford <loganstanford53@gmail.com>
2023-08-11 14:04:43 -0400

    ahhhhh

--- bot.py
+++ bot.py
@@ -82,12 +82,11 @@ async def delete_event(db, message, event_id):
 
 
 
-async def run_discord_bot(mongo_client, db):
+def run_discord_bot(mongo_client, db):
     TOKEN = 'MTEzMDIyNTQzNjAwNjMwNTk0Ng.GNqc6p.qR6t7fym71pGd3CLl9QKwQ8usCFoXhhG8W7PDE'
     intents = discord.Intents.all()
     intents.message_content = True
     client = discord.Client(intents=intents)
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='!help'))
 
     MY_ID = 1112204092723441724
     GUILD_ID = 1130553449491210442
@@ -96,6 +95,7 @@ async def run_discord_bot(mongo_client, db):
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='!help'))
 
     @client.event
     async def on_member_join(member):
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import bot
-import asyncio  # Import asyncio to handle asynchronous tasks
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
 uri = "mongodb+srv://loganstanford53:cMczqREMdzhQR9T6@cluster0.o9naf24.mongodb.net/?retryWrites=true&w=majority"
 
-async def main():
+
+
+if __name__ == '__main__':
+
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = None
     try:
@@ -20,12 +22,9 @@ async def main():
         # events = db['events']
         # events.delete_many({})
 
+        
     except Exception as e:
         print(e)
 
     print(db)
-    await bot.run_discord_bot(client, db)
-
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()  # Get the event loop
-    loop.run_until_complete(main())  # Run the asynchronous main function within the event loop
+    bot.run_discord_bot(client, db)
