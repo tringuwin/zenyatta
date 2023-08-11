07410102ba3101a5c5393032ab2c4e8708a7bf8c
Logan Stanford <loganstanford53@gmail.com>
2023-08-11 13:59:54 -0400

    async

--- bot.py
+++ bot.py
@@ -82,12 +82,12 @@ async def delete_event(db, message, event_id):
 
 
 
-def run_discord_bot(mongo_client, db):
+async def run_discord_bot(mongo_client, db):
     TOKEN = 'MTEzMDIyNTQzNjAwNjMwNTk0Ng.GNqc6p.qR6t7fym71pGd3CLl9QKwQ8usCFoXhhG8W7PDE'
     intents = discord.Intents.all()
     intents.message_content = True
     client = discord.Client(intents=intents)
-    client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='!help'))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='!help'))
 
     MY_ID = 1112204092723441724
     GUILD_ID = 1130553449491210442
