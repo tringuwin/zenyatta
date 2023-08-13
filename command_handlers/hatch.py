d4959c9649c98cac786bf83e613b83db26059a25
Logan Stanford <loganstanford53@gmail.com>
2023-08-13 14:34:57 -0400

    send embed

--- command_handlers/hatch.py
+++ command_handlers/hatch.py
@@ -2,6 +2,7 @@
 from common_messages import not_registered_response
 from rewards import change_eggs
 from user import user_exists
+import discord
 import random
 
 
@@ -20,7 +21,9 @@ async def hatch_handler(db, message):
         if 'eggs' in user and user['eggs'] > 0:
             await change_eggs(db, user, -1)
             creature_id = get_creature_id()
-            await message.channel.send('You hatched creature '+str(creature_id))
+            creature_embed = discord.Embed(title='You hacthed creature '+str(creature_id))
+            creature_embed.set_image('spicemon\S1.png')
+            await message.channel.send(embed = creature_embed)
         else:
             await message.channel.send('You do not have any eggs right now.')
 
