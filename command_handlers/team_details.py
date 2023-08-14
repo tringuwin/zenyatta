b559beb88f1f51fda799b694999778a60cae7a7b
Logan Stanford <loganstanford53@gmail.com>
2023-08-14 16:07:57 -0400

    team details

--- command_handlers/team_details.py
+++ command_handlers/team_details.py
@@ -7,8 +7,31 @@ def make_details_from_team(db, team):
     
     team_owner = user_exists(db, team['creator_id'])
 
-    team_string = '**Team Owner:** '+team_owner['battle_tag']+'\n'
+    team_string = '**'+team['team_name']+'** Details\n'
+    team_string += '-----------------------------------\n'
+    team_string += '**Team Owner:** '+team_owner['battle_tag']+'\n'
     team_string += '**Team Size:** '+str(team['team_size'])+'\n'
+    team_string += '**Number of Players:** '+str(len(team['members']))+'/'+str(team['team_size'])+'\n'
+    team_string += '-----------------------------------\n'
+    team_string += 'PLAYERS\n'
+    team_string += '-----------------------------------\n'
+
+    p_index = 0
+    while p_index < team['team_size']:
+        player = None
+        if p_index == 0:
+            player = team_owner
+        elif len(team['members']) > p_index :
+            player = user_exists(db, team['members'][p_index])
+
+
+        team_string += '**['+str(p_index+1)+']** '
+        if player:
+            team_string += player['battle_tag']+'\n'
+        else:
+            team_string += '*EMPTY*'
+        p_index += 1
+        
 
     return team_string
 
