c790eaafbb0a8d130e655ea7a9af9d127030a331
Logan Stanford <loganstanford53@gmail.com>
2023-08-16 16:08:13 -0400

    help update

--- command_handlers/help.py
+++ command_handlers/help.py
@@ -14,7 +14,7 @@ async def help_hanlder(message):
     help_embed.add_field(name='!tokens', value='See your current number of tokens', inline=False)
     help_embed.add_field(name='!passes', value='See your current passes', inline=False)
     help_embed.add_field(name='!sellpass', value='Sell 1 Priority Pass for 10 tokens', inline=False)
-    help_embed.add_field(name='!dailygift', value='Earn a daily gift once per day', inline=False)
+    help_embed.add_field(name='!gift', value='Earn a free gift every 8 hours!', inline=False)
     help_embed.add_field(name='!funfact [fun fact here]', value='Add a fun fact about yourself that might be mentioned during livestreamed events', inline=False)
     help_embed.add_field(name='!hello', value='Say hi to the Zenyatta bot', inline=False)
 
