ff40909e820698642d15e2d3bf0286b553abeb4a
Logan Stanford <loganstanford53@gmail.com>
2023-08-16 19:42:02 -0400

    edit messages

--- admin_handlers/update_shop.py
+++ admin_handlers/update_shop.py
@@ -7,13 +7,12 @@ async def get_shop_channel(message):
     return channel
 
 
-async def delete_shop_message_if_exists(the_shop, channel, message_id_label):
+async def get_shop_message(the_shop, channel, message_id_label):
     
     if message_id_label in the_shop:
 
-        del_msg = await channel.fetch_message(the_shop[message_id_label])
-        if del_msg:
-            await del_msg.delete()
+        shop_msg = await channel.fetch_message(the_shop[message_id_label])
+        return shop_msg
 
 async def update_shop_handler(db, message):
 
@@ -22,17 +21,20 @@ async def update_shop_handler(db, message):
     the_shop = shop.find_one({'shop_id': 1})
 
     channel = await get_shop_channel(message)
-    await delete_shop_message_if_exists(the_shop, channel, 'offers_message_id')
-    await delete_shop_message_if_exists(the_shop, channel, 'guide_message_id')
+    offer_msg = await get_shop_message(the_shop, channel, 'offers_message_id')
+    guide_msg = await get_shop_message(the_shop, channel, 'guide_message_id')
 
-    offers_string = 'this is the offers string'
-    guide_string = 'this is the guide string'
+    offers_string = 'this is the offers string [edited]'
+    guide_string = 'this is the guide string [EDITED]'
 
     offer_msg = await channel.send(offers_string)
     guide_msg = await channel.send(guide_string)
 
-    shop.update_one({"shop_id": 1}, {"$set": {"offers_message_id": offer_msg.id}})
-    shop.update_one({"shop_id": 1}, {"$set": {"guide_message_id": guide_msg.id}})
+    await offer_msg.edit(content=offers_string)
+    await guide_msg.edit(content=guide_msg)
+
+    # shop.update_one({"shop_id": 1}, {"$set": {"offers_message_id": offer_msg.id}})
+    # shop.update_one({"shop_id": 1}, {"$set": {"guide_message_id": guide_msg.id}})
 
     for offer in the_shop['offers']:
         await message.channel.send(offer['item_name'])
