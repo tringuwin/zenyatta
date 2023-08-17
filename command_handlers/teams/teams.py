d2a4d1c27277e1426751b327dcf3f97fe890c308
Logan Stanford <loganstanford53@gmail.com>
2023-08-17 14:18:45 -0400

    fix active teams

--- command_handlers/teams/teams.py
+++ command_handlers/teams/teams.py
@@ -3,6 +3,16 @@ from common_messages import not_registered_response
 from teams import get_team_by_name
 from user import user_exists
 
+def get_active_teams(team_names):
+
+    active_teams = []
+
+    for team_name in team_names:
+        team = get_team_by_name(team_name)
+        if team:
+            active_teams.append(team)
+
+
 
 async def teams_handler(db, message):
 
@@ -13,15 +23,16 @@ async def teams_handler(db, message):
     
     team_names = user['teams']
 
+    active_teams = get_active_teams(team_names)
+
     team_index = 1
-    if len(team_names) == 0:
+    if len(active_teams) == 0:
         await message.channel.send('You are not currently part of any teams.')
     else:
         output_string = '**YOUR TEAMS**\n'
-        for team_name in team_names:
-            team = await get_team_by_name(db, team_name)
-            if team:
-                output_string += str(team_index)+'. '+team_name+' : '+str(len(team['members']))+'/'+str(team['team_size'])+' Players\n'
-                team_index += 1
+        for team in active_teams:
+
+            output_string += str(team_index)+'. '+team['team_name']+' : '+str(len(team['members']))+'/'+str(team['team_size'])+' Players\n'
+            team_index += 1
 
         await message.channel.send(output_string)
