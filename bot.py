6ee654a511054f5ec70b90ab5c6159084fbe7bde
Logan Stanford <loganstanford53@gmail.com>
2023-08-18 16:22:37 -0400

    update mongo

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ from command_handlers.wager import wager_handler
 import constants
 from bracket import both_no_show, gen_tourney, no_show, notify_next_users, send_next_info, wipe_tourney, won_match
 from events import get_event_team_size
-from mongo import add_fun_fact, approve_user, create_event, create_or_update_battle_tag, deny_user, event_status, find_user_with_battle_tag, generate_bracket, get_all_events, get_event_by_id, give_daily_gift, output_eggs, output_passes, output_tokens, switch_matches
+from mongo import add_fun_fact, approve_user, create_event, create_or_update_battle_tag, deny_user, find_user_with_battle_tag, generate_bracket, get_all_events, get_event_by_id, give_daily_gift, output_eggs, output_passes, output_tokens, switch_matches
 from rewards import give_eggs_command, give_passes_command, change_tokens, give_tokens_command, sell_pass_for_tokens
 from user import user_exists
 
