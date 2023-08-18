6777580295f56efb9e694083ed384fa64d5b2197
Logan Stanford <loganstanford53@gmail.com>
2023-08-18 17:34:05 -0400

    add wager to help

--- command_handlers/help.py
+++ command_handlers/help.py
@@ -15,6 +15,7 @@ async def help_hanlder(message):
     help_embed.add_field(name='!passes', value='See your current passes', inline=False)
     help_embed.add_field(name='!sellpass', value='Sell 1 Priority Pass for 10 tokens', inline=False)
     help_embed.add_field(name='!gift', value='Earn a free gift every 8 hours!', inline=False)
+    help_embed.add_field(name='!wager [number of tokens] [red, black, or green]', value='Use in the casino channel. Wager your tokens with European Roulette rules.', inline=False)
     help_embed.add_field(name='!funfact [fun fact here]', value='Add a fun fact about yourself that might be mentioned during livestreamed events', inline=False)
     help_embed.add_field(name='!hello', value='Say hi to the Zenyatta bot', inline=False)
 
