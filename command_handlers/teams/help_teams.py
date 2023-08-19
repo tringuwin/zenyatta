afb5ef23a6b429b710ed9c8a07faf9c89bcdd57d
Logan Stanford <loganstanford53@gmail.com>
2023-08-19 18:41:04 -0400

    team join in help

--- command_handlers/teams/help_teams.py
+++ command_handlers/teams/help_teams.py
@@ -10,9 +10,9 @@ async def help_teams_hanlder(message):
     help_embed.add_field(name='!myinvites', value='See a list of teams you are invited to.', inline=False)
     help_embed.add_field(name='!acceptinvite [team name]', value='Accept an invite to a team.', inline=False)
     help_embed.add_field(name='!denyinvite [team name]', value='Deny an invite to a team.', inline=False)
+    help_embed.add_field(name='!teamjoin [event id] [team name]', value='Join an event as a team.', inline=False)
     help_embed.add_field(name='!leaveteam [team name]', value='Leave a team.', inline=False)
     help_embed.add_field(name='!deleteteam [team name]', value='Delete a team you own.', inline=False)
-    #help_embed.add_field(name='!teamjoin [event id] [team name]', value='Join an event as a team.', inline=False)
     help_embed.add_field(name='!kickplayer [@player] [team name]', value='Kick a player from a team you own.', inline=False)
 
     await message.channel.send(embed=help_embed)
