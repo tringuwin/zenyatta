1343483f34a439fe656cbe992e3186a679d2826f
Logan Stanford <loganstanford53@gmail.com>
2023-08-19 13:10:05 -0400

    await

--- command_handlers/teams/kick_player.py
+++ command_handlers/teams/kick_player.py
@@ -18,7 +18,7 @@ async def kick_player_handler(db, message):
         return
     
     team_name = make_string_from_word_list(word_list, 2)
-    team = get_team_by_name(db, team_name)
+    team = await get_team_by_name(db, team_name)
     if not team:
         await message.channel.send('There is no team with that name.')
         return
