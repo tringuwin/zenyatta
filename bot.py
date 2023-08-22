93f278ebfec5b633b6e70c64fac20a183c79c0d6
Logan Stanford <loganstanford53@gmail.com>
2023-08-22 13:17:56 -0400

    don't await get channel

--- bot.py
+++ bot.py
@@ -615,7 +615,7 @@ def run_discord_bot(db):
                 ]
                     
                 guild = await get_guild(client)
-                channel = await guild.get_channel(1143592783999926404)
+                channel = guild.get_channel(1143592783999926404)
                 for role in reaction_roles:
                     message = await channel.message.send('React to remove '+role['title']+ ' role.')
                     await message.add_reaction("❌")
