1b8532bee72899e249a06748c2b305a22c1abf3a
Logan Stanford <loganstanford53@gmail.com>
2023-09-07 18:10:05 -0400

    step 2

--- command_handlers/teams/del_team_from_event.py
+++ command_handlers/teams/del_team_from_event.py
@@ -19,9 +19,13 @@ async def del_team_from_event_handler(db, message):
     team_name = make_string_from_word_list(word_list, 2)
     team_name_lower = team_name.lower()
     entries = event['entries']
+    final_entries = []
+    print(str(len(entries)))
     for entry in entries:
-        if entry.lower() == team_name_lower:
-            print('we should remove '+entry)
+        if entry.lower() != team_name_lower:
+            final_entries.append(entry)
+    print(str(len(final_entries)))
 
-
+    # events = db['events']
+    # events.update_one({"event_id": event['event_id']}, {"$set": {"entries": final_entries, "spots_filled": len(final_entries)}})
     await message.channel.send('command done')
