5324a666429075c0d3f3b739dce881790d6958b4
Logan Stanford <loganstanford53@gmail.com>
2023-09-08 01:01:49 -0400

    better gifts for subs

--- command_handlers/gift.py
+++ command_handlers/gift.py
@@ -1,26 +1,44 @@
 from common_messages import not_registered_response
+from discord_actions import member_has_role
 from rewards import change_passes, change_tokens
 from time_helpers import get_current_time, long_enough_for_gift, time_to_gift
 from user import user_exists
 import random
+import constants
 
 
 async def process_gift(db, current_time, existing_user, message):
+
+    is_sub = member_has_role(message.author, constants.TWITCH_SUB_ROLE)
+
     users = db['users']
     users.update_one({"discord_id": existing_user['discord_id']}, {"$set": {"last_gift": current_time, "gift_notify": True}})
     general_info = '\n*Come back in 8 hours for another gift!*'
 
     prize_index = random.randint(1, 100)
-    if prize_index == 1:
-        await change_tokens(db, existing_user, 100)
-        await message.channel.send(message.author.mention+" 🪙 **YOU FOUND 100 TOKENS!!** 🪙"+general_info)
-    elif prize_index <= 10:
-        await change_passes(db, existing_user, 1)
-        await message.channel.send(message.author.mention+" 🎟️ You found a **Priority Pass!** 🎟️"+general_info)
+    if is_sub:
+        general_info += "\n~ Since you're a Twitch Sub, you get better gift rewards! ~"
+        if prize_index <= 3:
+            await change_tokens(db, existing_user, 100)
+            await message.channel.send(message.author.mention+" 🪙 **YOU FOUND 100 TOKENS!!** 🪙"+general_info)
+        elif prize_index <= 15:
+            await change_passes(db, existing_user, 1)
+            await message.channel.send(message.author.mention+" 🎟️ You found a **Priority Pass!** 🎟️"+general_info)
+        else:
+            tokens = random.randint(10, 20)
+            await change_tokens(db, existing_user, tokens)
+            await message.channel.send(message.author.mention+" 🪙 You found **"+ str(tokens)+" Tokens** 🪙"+general_info)
     else:
-        tokens = random.randint(2, 5)
-        await change_tokens(db, existing_user, tokens)
-        await message.channel.send(message.author.mention+" 🪙 You found **"+ str(tokens)+" Tokens** 🪙"+general_info)
+        if prize_index == 1:
+            await change_tokens(db, existing_user, 100)
+            await message.channel.send(message.author.mention+" 🪙 **YOU FOUND 100 TOKENS!!** 🪙"+general_info)
+        elif prize_index <= 10:
+            await change_passes(db, existing_user, 1)
+            await message.channel.send(message.author.mention+" 🎟️ You found a **Priority Pass!** 🎟️"+general_info)
+        else:
+            tokens = random.randint(2, 5)
+            await change_tokens(db, existing_user, tokens)
+            await message.channel.send(message.author.mention+" 🪙 You found **"+ str(tokens)+" Tokens** 🪙"+general_info)
 
 
 
