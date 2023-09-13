e2c7af81716b773d4cc8289742b37972a2b1f3ea
Logan Stanford <loganstanford53@gmail.com>
2023-09-13 15:48:41 -0400

    poop soup

--- command_handlers/wager.py
+++ command_handlers/wager.py
@@ -102,7 +102,7 @@ async def wager_handler(db, message):
             return
         wager = int(wager)
 
-        if wager > 100 or wager < 1:
+        if wager > 600 or wager < 1:
             await message.channel.send('Wager must be between 1 and 100 tokens')
             return
         
