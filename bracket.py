453b86019f7900834d5b5daad9fdaec81cff8f15
Logan Stanford <loganstanford53@gmail.com>
2023-10-07 11:16:20 -0400

    force event channel

--- bracket.py
+++ bracket.py
@@ -261,7 +261,7 @@ async def notify_next_users(db, guild, message):
             else:
                 break
         final_string += '\n--------------------------------------------'
-        event_channel = guild.get_channel(1157408569247936563)
+        event_channel = guild.get_channel(1158851247193989160)
         await event_channel.send(final_string)
         
     else:
