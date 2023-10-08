1ab26870617f8a66986cbf0e3fc30cb007b9cad0
Logan Stanford <loganstanford53@gmail.com>
2023-10-08 12:49:23 -0400

    better

--- bracket.py
+++ bracket.py
@@ -204,6 +204,8 @@ async def notify_match(match, start_string, guild, db):
     if side1['is_bye'] and side2['is_bye']:
         return start_string+' *Empty Match*'
     elif side1['is_bye']:
+        print(type(start_string))
+        print(type(side2['user']))
         return start_string+" "+side2['user'] + ' will recieve a bye and advance to the next round.'
     elif side2['is_bye']:
         return start_string+" "+side1['user'] + ' will recieve a bye and advance to the next round.'
