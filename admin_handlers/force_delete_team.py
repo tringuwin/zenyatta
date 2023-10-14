113c9cf1e164d63c26a3466bae10b7638fed948b
Logan Stanford <loganstanford53@gmail.com>
2023-10-14 19:44:06 -0400

    fix

--- admin_handlers/force_delete_team.py
+++ admin_handlers/force_delete_team.py
@@ -7,7 +7,7 @@ from teams import get_team_by_name
 async def force_delete_team_handler(db, message, client):
 
     word_list = message.content.split()
-    team_name = make_string_from_word_list(word_list, 2)
+    team_name = make_string_from_word_list(word_list, 1)
 
     team = await get_team_by_name(db, team_name)
     if not team:
