84a5f355a9c1926caa4d59f12eaded59807e3ecd
Logan Stanford <loganstanford53@gmail.com>
2023-10-22 19:28:39 -0400

    fuc

--- bot.py
+++ bot.py
@@ -413,9 +413,9 @@ async def handle_message(message, db, client):
 
     elif lower_message.startswith('!giverewards') and is_admin:
         
-        reward_per_round = [100, 200, 300, 500, 700, 1000, 2000]
+        reward_per_round = [50, 100, 200, 300, 500, 1000, 2000]
 
-        bracket = db['brackets'].find_one({'event_id': '10'})
+        bracket = db['brackets'].find_one({'event_id': '11'})
 
         final_dict = {}
 
@@ -467,7 +467,7 @@ async def handle_message(message, db, client):
                 if user:
 
                     reward = reward_per_round[highest_round]
-                    await change_tokens(db, user, reward)
+                    #await change_tokens(db, user, reward)
                     print('Giving '+str(reward)+' tokens to '+user['battle_tag'])
 
         await message.channel.send('Rewards given')
