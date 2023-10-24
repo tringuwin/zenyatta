527a17d043cfe5c903dc8c592678441e3490eb61
Logan Stanford <loganstanford53@gmail.com>
2023-10-24 16:35:35 -0400

    fix

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ async def handle_message(message, db, client):
     user_message = str(message.content)
     print(user_message)
     is_admin = (message.author.id == constants.SPICY_RAGU_ID)
-    is_helper = (not member.bot) and member_has_role(message.author, constants.HELPER_ROLE_ID)
+    is_helper = (not message.author.bot) and member_has_role(message.author, constants.HELPER_ROLE_ID)
     is_command = len(user_message) > 0 and (user_message[0] == '!')
     if not is_command:
         return
