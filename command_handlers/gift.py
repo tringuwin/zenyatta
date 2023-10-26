db7950d2e72a4a1e6e774bd7153bf2be44af994a
Logan Stanford <loganstanford53@gmail.com>
2023-10-26 13:39:44 -0400

    cum

--- command_handlers/gift.py
+++ command_handlers/gift.py
@@ -7,7 +7,7 @@ import random
 import constants
 
 
-async def give_gem(db, message, user, end_string, client):
+async def give_gem(db, user, client):
 
     random_gem = random.choice(constants.GEM_COLORS)
     user_gems = get_user_gems(user)
@@ -20,7 +20,7 @@ async def give_gem(db, message, user, end_string, client):
     gem_emoji_id = constants.COLOR_TO_EMOJI_ID[random_gem]
     gem_emoji = guild.get_emoji(gem_emoji_id)
 
-    await message.channel.send(message.author.mention+" "+str(gem_emoji)+' **You found a '+random_gem+' gem!!** '+str(gem_emoji)+end_string)
+    return str(gem_emoji)+' **You found a '+random_gem+' gem!!** '+str(gem_emoji)
     
 
 
@@ -32,42 +32,55 @@ async def process_gift(db, current_time, existing_user, message, client):
 
     users = db['users']
     users.update_one({"discord_id": existing_user['discord_id']}, {"$set": {"last_gift": current_time, "gift_notify": True}})
-    general_info = '\n*Come back in 8 hours for another gift!*'
+    message_string = message.author.mention+' '
+
+    total_tokens_to_give = 0
 
     prize_index = random.randint(1, 100)
     if is_sub:
-        general_info += "\n~ Since you're a Twitch Sub, you get better gift rewards! ~"
         if prize_index <= 3:
-            await change_tokens(db, existing_user, 100)
-            await message.channel.send(message.author.mention+" 🪙 **YOU FOUND 100 TOKENS!!** 🪙"+general_info)
+            total_tokens_to_give += 100
+            message_string += "🪙 **YOU FOUND 100 TOKENS!!** 🪙"
         elif prize_index <= 9:
             await change_passes(db, existing_user, 1)
-            await message.channel.send(message.author.mention+" 🎟️ You found a **Priority Pass!** 🎟️"+general_info)
+            message_string += '🎟️ You found a **Priority Pass!** 🎟️'
         elif prize_index <= 20:
             await change_pickaxes(db, existing_user, 1)
-            await message.channel.send(message.author.mention+" ⛏️ You found a **Pickaxe!** ⛏️ Use it in the Mineshaft! "+general_info)
+            message_string += "⛏️ You found a **Pickaxe!** ⛏️ Use it in the Mineshaft!"
         elif prize_index <= 30:
-            await give_gem(db, message, existing_user, general_info, client)
+            message_part = await give_gem(db, existing_user, client)
+            message_string += message_part
         else:
             tokens = random.randint(10, 20)
-            await change_tokens(db, existing_user, tokens)
-            await message.channel.send(message.author.mention+" 🪙 You found **"+ str(tokens)+" Tokens** 🪙"+general_info)
+            total_tokens_to_give += tokens
+            message_string += "🪙 You found **"+ str(tokens)+" Tokens** 🪙"
     else:
         if prize_index == 1:
-            await change_tokens(db, existing_user, 100)
-            await message.channel.send(message.author.mention+" 🪙 **YOU FOUND 100 TOKENS!!** 🪙"+general_info)
+            total_tokens_to_give += 100
+            message_string += "🪙 **YOU FOUND 100 TOKENS!!** 🪙"
         elif prize_index <= 5:
             await change_passes(db, existing_user, 1)
-            await message.channel.send(message.author.mention+" 🎟️ You found a **Priority Pass!** 🎟️"+general_info)
+            message_string += '🎟️ You found a **Priority Pass!** 🎟️'
         elif prize_index <= 10:
             await change_pickaxes(db, existing_user, 1)
-            await message.channel.send(message.author.mention+" ⛏️ You found a **Pickaxe!** ⛏️ Use it in the Mineshaft! "+general_info)
+            message_string += "⛏️ You found a **Pickaxe!** ⛏️ Use it in the Mineshaft!"
         elif prize_index <= 15:
-            await give_gem(db, message, existing_user, "", client)
+            message_part = await give_gem(db, existing_user, client)
+            message_string += message_part
         else:
             tokens = random.randint(2, 5)
-            await change_tokens(db, existing_user, tokens)
-            await message.channel.send(message.author.mention+" 🪙 You found **"+ str(tokens)+" Tokens** 🪙"+general_info)
+            total_tokens_to_give += 100
+            message_string += "🪙 You found **"+ str(tokens)+" Tokens** 🪙"
+
+    if total_tokens_to_give > 0:
+        await change_tokens(db, existing_user, total_tokens_to_give)
+
+
+    if is_sub:
+        message_string += "\n~ Since you're a Twitch Sub, you get better gift rewards! ~"
+    message_string += '\n*Come back in 8 hours for another gift!*'
+
+    await message.channel.send(message_string)
 
 
 
