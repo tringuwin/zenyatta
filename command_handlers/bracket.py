71f3ee5a2810bac026fb6842a8f17293cb7a892d
Logan Stanford <loganstanford53@gmail.com>
2023-10-29 19:52:17 -0400

    balls

--- command_handlers/bracket.py
+++ command_handlers/bracket.py
@@ -1,4 +1,4 @@
 
 async def bracket_handler(message):
 
-    message.channel.send(message.author.mention+' Bracket for the current/next tournament:\nhttps://spicyragu.netlify.app/bracket')
+    await message.channel.send(message.author.mention+' Bracket for the current/next tournament:\nhttps://spicyragu.netlify.app/bracket')
