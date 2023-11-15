4c5734df5a2463b90cede79c7e792fc1dfa5a061
Logan Stanford <loganstanford53@gmail.com>
2023-11-14 19:49:49 -0500

    nutsacker

--- command_handlers/help/help_bonus.py
+++ command_handlers/help/help_bonus.py
@@ -4,6 +4,7 @@ import discord
 async def help_bonus_handler(message):
     
     help_embed = discord.Embed(title='List of bonus commands:')
+    help_embed.add_field(name='!tokenleaderboard', value='See the top 10 users with the most tokens in the server.', inline=False)
     help_embed.add_field(name='!hello', value='Say hi to the Zenyatta bot', inline=False)
     help_embed.add_field(name='!gg ez', value='Zenyatta will respond with one of the classic "gg ez" responses.', inline=False)
     help_embed.add_field(name='!whichhero [question]', value='Ask the Zenyatta bot a question and it will respond with a hero. (Example: !whichhero should be nerfed?)', inline=False)
