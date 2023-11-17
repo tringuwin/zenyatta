caf3cc6606dcaea8a12b8fea4803b85a04f40e1a
Logan Stanford <loganstanford53@gmail.com>
2023-11-17 13:26:26 -0500

    pengus

--- admin_handlers/give_sub_boxes.py
+++ admin_handlers/give_sub_boxes.py
@@ -1,7 +1,7 @@
 
 from discord_actions import get_guild, get_role_by_id
 import constants
-from user import get_last_sub_box, user_exists
+from user import get_last_sub_box, get_sub_lootboxes, user_exists
 import time
 
 TIME_IN_30_DAYS = 2592000
@@ -20,10 +20,12 @@ async def give_sub_boxes_handler(db, message, client):
             user = user_exists(db, member.id)
             if user:
                 last_sub_box = get_last_sub_box(user)
+                user_boxes = get_sub_lootboxes(user)
                 current_time = time.time()
                 difference = current_time - last_sub_box
                 if difference >= TIME_IN_30_DAYS:
                     boxes_given += 1
-                    users.update_one({"discord_id": user['discord_id']}, {"$set": {"sub_lootboxes": user['sub_lootboxes']+1, 'last_sub_box': current_time}})
+                    user_boxes += 1
+                    users.update_one({"discord_id": user['discord_id']}, {"$set": {"sub_lootboxes": user_boxes, 'last_sub_box': current_time}})
 
     await message.channel.send('Command complete. '+str(boxes_given)+' total twitch lootboxes given.')
