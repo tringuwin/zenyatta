db49bc21adc2bf3915d020dfb74c9e59db762999
Logan Stanford <loganstanford53@gmail.com>
2023-11-20 16:13:41 -0500

    aids

--- command_handlers/standings.py
+++ command_handlers/standings.py
@@ -10,6 +10,7 @@ async def standings_handler(db, message):
     standings = db['standings']
     season_object = standings.find_one({'season': season})
 
+    final_teams = {}
     teams = season_object['teams']
     for team_name in teams:
         team = teams[team_name]
@@ -19,9 +20,12 @@ async def standings_handler(db, message):
             win_percent = float(team[0]) / float(total_matches)
             win_percent = str(round(win_percent, 2))
 
-        team['win_percent'] = win_percent
+        final_teams[team_name] = {
+            'team': team,
+            'win_percent': win_percent
+        }
 
-    sorted_teams = sorted(teams, key=lambda x: x["win_percent"], reverse=True)
+    sorted_teams = sorted(final_teams, key=lambda x: x["win_percent"], reverse=True)
     final_string = '**LEAGUE STANDINGS**\n-----------------------'
     for team_name in sorted_teams:
         final_string += '\n'+pad_string_to_length(team_name, 20)+'|'
