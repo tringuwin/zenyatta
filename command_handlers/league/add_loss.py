df2dade58507c6f03272801e45b9e2f973861da5
Logan Stanford <loganstanford53@gmail.com>
2023-11-25 18:44:44 -0500

    add loss

---
+++ command_handlers/league/add_loss.py
@@ -0,0 +1,25 @@
+
+from common_messages import invalid_number_of_params
+from helpers import make_string_from_word_list
+
+import constants
+
+async def add_loss_handler(db, message):
+
+    word_parts = message.content.split()
+    if len(word_parts) < 2:
+        await invalid_number_of_params(message)
+        return
+    
+    team_name = make_string_from_word_list(word_parts, 1)
+    if not team_name in constants.TEAM_LIST:
+        await message.channel.send(team_name+' is not a valid team name')
+        return
+    
+    standings = db['standings']
+    standings_obj = standings.find_one({'season': 1})
+    standings_obj['teams'][team_name][1] += 1
+
+    standings.update_one({"season": 1}, {"$set": {"teams": standings_obj['teams']}})
+
+    await message.channel.send('team loss added')
