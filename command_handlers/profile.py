b079d7fa92cfcf29a16323946fcbf9e60de3799f
Logan Stanford <loganstanford53@gmail.com>
2023-11-29 18:56:57 -0500

    buns

--- command_handlers/profile.py
+++ command_handlers/profile.py
@@ -4,6 +4,9 @@ from helpers import make_string_from_word_list
 from user import get_fan_of, get_league_team, get_lvl_info, get_rival_of, get_user_gems, get_user_passes, get_user_pickaxes, get_user_tokens, user_exists
 import constants
 
+
+
+
 async def profile_handler(db, message, client):
 
     word_list = message.content.split()
@@ -24,6 +27,7 @@ async def profile_handler(db, message, client):
         await message.channel.send('User not found.')
         return
     
+    guild = await get_guild(client)
 
     level, xp = get_lvl_info(user)
     league_team = get_league_team(user)
@@ -34,9 +38,28 @@ async def profile_handler(db, message, client):
     pickaxes = get_user_pickaxes(user)
     final_string = "**USER PROFILE FOR "+user['battle_tag']+':**\n'
     final_string += 'Level '+str(level)+' | XP: ('+str(xp)+'/'+str(level*100)+')\n'
-    final_string += 'League Team: **'+league_team+"**\n"
-    final_string += 'Fan of Team: **'+fan_of+'**\n'
-    final_string += 'Rival of Team: **'+rival_of+'**\n'
+
+    league_team_string = league_team
+    if league_team in constants.EMOJI_TEAMS:
+        team_emoji_id = constants.LEAGUE_TO_EMOJI_ID[league_team]
+        team_emoji = guild.get_emoji(team_emoji_id)
+        league_team_string = str(team_emoji)+' '+league_team_string
+
+    fan_of_string = fan_of
+    if fan_of in constants.EMOJI_TEAMS:
+        fan_emoji_id = constants.LEAGUE_TO_EMOJI_ID[fan_of]
+        fan_emoji = guild.get_emoji(fan_emoji_id)
+        fan_of_string = str(fan_emoji)+' '+fan_of_string
+
+    rival_of_string = rival_of
+    if league_team in constants.EMOJI_TEAMS:
+        rival_emoji_id = constants.LEAGUE_TO_EMOJI_ID[rival_of]
+        rival_emoji = guild.get_emoji(rival_emoji_id)
+        rival_of_string = str(rival_emoji)+' '+rival_of_string
+        
+    final_string += 'League Team: **'+league_team_string+"**\n"
+    final_string += 'Fan of Team: **'+fan_of_string+'**\n'
+    final_string += 'Rival of Team: **'+rival_of_string+'**\n'
 
     final_string +='\n'
     final_string += '🪙 '+str(tokens)+' 🎟️ '+str(passes)+' ⛏️ '+str(pickaxes)+'\n'
@@ -45,7 +68,6 @@ async def profile_handler(db, message, client):
     gem_line_1 = ''
     gem_line_2 = ''
 
-    guild = await get_guild(client)
     gem_index = 1
     for color, amount in gems.items():
         emoji_id = constants.COLOR_TO_EMOJI_ID[color]
