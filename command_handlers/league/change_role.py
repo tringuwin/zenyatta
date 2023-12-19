b60a0fa0765d35920ff26e47019651477a0b9855
Logan Stanford <loganstanford53@gmail.com>
2023-12-19 12:38:57 -0500

    change role uses username

--- command_handlers/league/change_role.py
+++ command_handlers/league/change_role.py
@@ -1,18 +1,16 @@
 
 from common_messages import invalid_number_of_params
-from helpers import make_string_from_word_list
+from discord_actions import get_member_by_username
+from helpers import generic_find_user, make_string_from_word_list
 from league import update_team_info, validate_admin
 import constants
+from user import user_exists
 
 async def change_role_handler(db, message, client):
 
     word_list = message.content.split()
     if len(word_list) < 3:
         await invalid_number_of_params(message)
-        return
-    
-    if len(message.mentions) < 1:
-        await message.channel.send('Please mention the player to change their role.')
         return
     
     is_admin, my_team, team_name, _ = await validate_admin(db, message)
@@ -22,12 +20,27 @@ async def change_role_handler(db, message, client):
         await message.channel.send('You are not an admin of this league team. Please ask the team owner to be an admin.')
         return
     
-    member_to_find = message.mentions[0]
+    user_to_change_role = None
+    member = None
+    username = None
+
+    if len(message.mentions) == 1:
+        user_to_change_role = user_exists(db, message.mentions[0].id)
+        member = message.mentions[0]
+    else:
+        username = word_list[1]
+        user_to_change_role = await generic_find_user(client, db, username)
+        member = await get_member_by_username(client, username)
+    
+    if not user_to_change_role:
+        await message.channel.send('User not found.')
+        return
+
     at_member = None
     at_member_index = 0
     cur_index = 0
     for member in team_members:
-        if member['discord_id'] == member_to_find.id:
+        if member['discord_id'] == user_to_change_role['discord_id']:
             at_member = member
             at_member_index = cur_index
             break
@@ -44,7 +57,11 @@ async def change_role_handler(db, message, client):
     league_teams.update_one({'team_name': team_name}, {"$set": {"members": my_team['members']}})
 
     league_notifs_channel = client.get_channel(constants.TEAM_NOTIFS_CHANNEL)
-    await league_notifs_channel.send('Team Update for '+team_name+": "+member_to_find.mention+"'s role has been changed to "+new_role)
+    if member:
+        await league_notifs_channel.send('Team Update for '+team_name+": "+member.mention+"'s role has been changed to "+new_role)
+    else:
+        await league_notifs_channel.send('Team Update for '+team_name+": "+username+"'s role has been changed to "+new_role)
+
 
     await update_team_info(client, my_team, db)
 
