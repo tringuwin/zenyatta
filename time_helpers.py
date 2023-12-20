0d4e8e213823effba44e4eb5389065395882cd85
Logan Stanford <loganstanford53@gmail.com>
2023-12-20 11:53:09 -0500

    no long deci

--- time_helpers.py
+++ time_helpers.py
@@ -28,23 +28,23 @@ def time_to_gift(diff_in_time):
 
 def time_to_shop(diff_in_time):
     
-    time_to_gift = constants.TIME_BETWEEN_SHOP - diff_in_time
+    time_to_shop = constants.TIME_BETWEEN_SHOP - diff_in_time
 
     days = 0
     hours = 0
     minutes = 0
 
-    while time_to_gift > 86400:
+    while time_to_shop > 86400:
         days += 1
-        time_to_gift -= 86400
-    while time_to_gift > 3600:
+        time_to_shop -= 86400
+    while time_to_shop > 3600:
         hours += 1
-        time_to_gift -= 3600
-    while time_to_gift >= 60:
+        time_to_shop -= 3600
+    while time_to_shop >= 60:
         minutes += 1
-        time_to_gift -= 60
+        time_to_shop -= 60
 
-    return format_time(days, 'days')+format_time(hours, 'hours')+format_time(minutes, 'minutes')+format_time(time_to_gift, 'seconds')
+    return format_time(days, 'days')+format_time(hours, 'hours')+format_time(minutes, 'minutes')+format_time(int(time_to_shop), 'seconds')
 
 
 def get_current_time():
