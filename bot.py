718e09015ee635ce621c29d79b44756d52a3cc98
Logan Stanford <loganstanford53@gmail.com>
2024-02-13 18:57:56 -0500

    balls

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ from admin_handlers.give_xp import give_xp_handler
 from admin_handlers.prune_sac import prune_sac_handler
 from admin_handlers.set_item_price import set_item_price_handler
 from admin_handlers.set_level import set_level_handler
+from cards import init_card_handler
 from command_handlers.accept_gem_trade import accept_gem_trade_handler
 from command_handlers.auction.bid import bid_handler
 from command_handlers.auction.end_auction import end_auction_handler
@@ -699,6 +700,10 @@ async def handle_message(message, db, client):
         }
         db_cards.insert_one(new_entry)
         await message.channel.send('init success')
+
+    elif lower_message.startswith('!initcard') and is_admin:
+
+        await init_card_handler(db, message)
 
     elif lower_message == '!resetraffle' and is_admin:
         db_constants = db['constants']
---
+++ cards_data.py
@@ -0,0 +1,325 @@
+ALL_PLAYERS = {
+
+    'spicy': 1112204092723441724,
+
+    'deja': 481300232986951680,
+    'robynn': 479430055424950292,
+    'squishey': 296781287140884491,
+    'dulla': 1104903934742642728,
+    'thaimermaid': 988882826512437258,
+    
+    'ionalien': 271540455584301057,
+    'v4mp': 980247078754222101,
+    'wendywilliams': 579333665264238592,
+    'allykat': 342459985550114816,
+    'ayudame': 199952617655238656,
+
+    'avernus': 410344325566955530,
+    'dripobito': 729081275318665267,
+    'valk': 841841724565487617,
+    'maya': 559508904430665730,
+    'bigmacmucher': 750843306191355926,
+
+    'randy': 396183118505181187,
+    'heluvasra': 992637058906603590,
+    'kale': 532336106364993538,
+    'miguelsweb': 900925608392024085,
+    'fluttershy': 1076905994526924881,
+
+    'anakin': 465416650083270667,
+    'truexotic': 581929894548733963,
+    'chris': 737917054132420678,
+    'heavenly': 1079552235526172672,
+    'trixx': 558664171252613151,
+
+    'ssolaced': 700150782606966859,
+    'cherrybee': 1110307252067389451,
+    'graded': 988593466869166120,
+    'luffy': 781732557217595412,
+    'buttermilk': 770799729487511552,
+
+    'mrbustdown': 712545218397732955,
+    'justtoosus': 787091730888917013,
+    'slushy': 408673719742103553,
+    'mangomemes': 801887866405126194,
+    'cheems': 694590519350919248,
+
+    'kuda': 534121688737120276,
+    'snoopy': 0,
+    'BigJ5': 451194679292198922,
+    'Bless': 311139185866047488,
+    'Kyrios': 0,
+
+    'bakora': 348133231620521985,
+    'masiax': 792954113104412722,
+    'kazzy': 513207840878624776,
+    'kittenmaster': 840415629008830534,
+    'g0ast': 703331500967592006,
+
+    'abyx': 1049667906369228844,
+    'jacob': 592828437131952130,
+    'grand': 704751495702380604,
+    'skylar': 1174175290612793409,
+    'boostio': 607788667833942035
+
+}
+
+ALL_CARDS = {
+
+    '0': {
+        'player_id': ALL_PLAYERS['spicy'],
+        'normal_img': 'https://i.postimg.cc/RCn0WtBW/spicy-card.jpg',
+        'special_img': 'https://i.postimg.cc/90TXK6pd/imageedit-1-8844141333.gif'
+    },
+    '1': {
+        'player_id': ALL_PLAYERS['deja'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '2': {
+        'player_id': ALL_PLAYERS['robynn'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '3': {
+        'player_id': ALL_PLAYERS['squishey'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '4': {
+        'player_id': ALL_PLAYERS['dulla'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '5': {
+        'player_id': ALL_PLAYERS['thaimermaid'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '6': {
+        'player_id': ALL_PLAYERS['ionalien'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '7': {
+        'player_id': ALL_PLAYERS['v4mp'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '8': {
+        'player_id': ALL_PLAYERS['wendywilliams'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '9': {
+        'player_id': ALL_PLAYERS['allykat'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '10': {
+        'player_id': ALL_PLAYERS['ayudame'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '11': {
+        'player_id': ALL_PLAYERS['avernus'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '12': {
+        'player_id': ALL_PLAYERS['dripobito'],
+        'normal_img': '',
+        'special_img': ''
+    }, 
+    '13': {
+        'player_id': ALL_PLAYERS['valk'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '14': {
+        'player_id': ALL_PLAYERS['maya'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '15': {
+        'player_id': ALL_PLAYERS['bigmacmucher'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '16': {
+        'player_id': ALL_PLAYERS['randy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '17': {
+        'player_id': ALL_PLAYERS['heluvasra'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '18': {
+        'player_id': ALL_PLAYERS['kale'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '19': {
+        'player_id': ALL_PLAYERS['miguelsweb'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '20': {
+        'player_id': ALL_PLAYERS['fluttershy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '21': {
+        'player_id': ALL_PLAYERS['anakin'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '22': {
+        'player_id': ALL_PLAYERS['truexotic'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '23': {
+        'player_id': ALL_PLAYERS['chris'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '24': {
+        'player_id': ALL_PLAYERS['heavenly'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '25': {
+        'player_id': ALL_PLAYERS['trixx'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '26': {
+        'player_id': ALL_PLAYERS['ssolaced'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '27': {
+        'player_id': ALL_PLAYERS['cherrybee'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '28': {
+        'player_id': ALL_PLAYERS['graded'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '29': {
+        'player_id': ALL_PLAYERS['luffy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '30': {
+        'player_id': ALL_PLAYERS['buttermilk'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '31': {
+        'player_id': ALL_PLAYERS['mrbustdown'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '32': {
+        'player_id': ALL_PLAYERS['justtoosus'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '33': {
+        'player_id': ALL_PLAYERS['slushy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '34': {
+        'player_id': ALL_PLAYERS['mangomemes'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '35': {
+        'player_id': ALL_PLAYERS['cheems'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '36': {
+        'player_id': ALL_PLAYERS['kuda'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '37': {
+        'player_id': ALL_PLAYERS['snoopy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '38': {
+        'player_id': ALL_PLAYERS['Bigj5'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '39': {
+        'player_id': ALL_PLAYERS['Bless'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '40': {
+        'player_id': ALL_PLAYERS['Kyrios'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '41': {
+        'player_id': ALL_PLAYERS['bakora'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '42': {
+        'player_id': ALL_PLAYERS['masiax'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '43': {
+        'player_id': ALL_PLAYERS['kazzy'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '44': {
+        'player_id': ALL_PLAYERS['kittenmaster'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '45': {
+        'player_id': ALL_PLAYERS['g0ast'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '46': {
+        'player_id': ALL_PLAYERS['abyx'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '47': {
+        'player_id': ALL_PLAYERS['jacob'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '48': {
+        'player_id': ALL_PLAYERS['grand'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '49': {
+        'player_id': ALL_PLAYERS['skylar'],
+        'normal_img': '',
+        'special_img': ''
+    },
+    '50': {
+        'player_id': ALL_PLAYERS['boostio'],
+        'normal_img': '',
+        'special_img': ''
+    },
+
+}
