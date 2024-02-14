c0be159396ff41ecb939be2f9653463423d0bee4
Logan Stanford <loganstanford53@gmail.com>
2024-02-14 14:40:51 -0500

    wipe

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ from admin_handlers.give_xp import give_xp_handler
 from admin_handlers.prune_sac import prune_sac_handler
 from admin_handlers.set_item_price import set_item_price_handler
 from admin_handlers.set_level import set_level_handler
-from cards import cards_handler, init_card_handler, open_pack_handler, wipe_card_database_handler
+from cards import cards_handler, init_card_handler, open_pack_handler, wipe_card_database_handler, wipe_player_cards_handler
 from command_handlers.accept_gem_trade import accept_gem_trade_handler
 from command_handlers.auction.bid import bid_handler
 from command_handlers.auction.end_auction import end_auction_handler
@@ -710,6 +710,9 @@ async def handle_message(message, db, client):
 
     elif lower_message == '!wipecarddatabase' and is_admin:
         await wipe_card_database_handler(db, message)
+
+    elif lower_message.startswith('!wipeplayercards ') and is_admin:
+        await wipe_player_cards_handler(db, message)
 
     elif lower_message.startswith('!initcard ') and is_admin:
 
