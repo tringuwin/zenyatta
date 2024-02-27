45ed9945b5d612c9179c3a3825d731a364c6c521
Logan Stanford <loganstanford53@gmail.com>
2024-02-26 20:34:41 -0500

    help cards

--- command_handlers/help/help_bonus.py
+++ command_handlers/help/help_bonus.py
@@ -6,6 +6,7 @@ async def help_bonus_handler(message):
     help_embed = discord.Embed(title='List of bonus commands:')
     help_embed.add_field(name='!auctiontimer', value='Shows how much time is left until the Daily Auction ends.', inline=False)
     help_embed.add_field(name='!subtimer', value='Shows how many days are left for your Twitch Sub Lootbox (if you are a Twitch Sub).', inline=False)
+    help_embed.add_field(name='!leaderboard', value='Shows the Top 10 players by Level/XP and links to the full server leaderboard.', inline=False)
     help_embed.add_field(name='!tokenleaderboard', value='See the top 10 users with the most tokens in the server.', inline=False)
     help_embed.add_field(name='!hello', value='Say hi to the Zenyatta bot', inline=False)
     help_embed.add_field(name='!gg ez', value='Zenyatta will respond with one of the classic "gg ez" responses.', inline=False)
