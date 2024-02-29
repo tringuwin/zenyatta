40ede88c3079743aa6b9647e55bdbfa4317e7d9e
Logan Stanford <loganstanford53@gmail.com>
2024-02-29 17:40:30 -0500

    fuck

--- command_handlers/help/help_league.py
+++ command_handlers/help/help_league.py
@@ -11,7 +11,7 @@ async def help_league_handler(message):
     help_embed.add_field(name='!leaguecancelinvite @User', value="Admin command to cancel a user's pending invite to your team.", inline=False)
     help_embed.add_field(name='!leagueinvites', value='See a list of league teams that you are invited to.', inline=False)
     help_embed.add_field(name='!leagueaccept [team name]', value='Accept an invite to join a league team. Please make sure to use exact team name and capitilization.', inline=False)
-    help_embed.add_field(name='!changetpp @Player [tpp number]', value='Admin command to change the TPP of a player on your league team.', inline=False)
+    help_embed.add_field(name='!changetpp username [tpp number]', value='Admin command to change the TPP of a player on your league team.', inline=False)
     help_embed.add_field(name='!changerole @Player [role info]', value='Admin command to change the role of a player on your league team.', inline=False)
     help_embed.add_field(name='!maketeamadmin @Player', value='Owner command to make a member of your league team an admin.', inline=False)
     help_embed.add_field(name='!removeteamadmin @Player', value='Owner command to remove admin role from a member of your league team.', inline=False)
--- command_handlers/league/change_tpp.py
+++ command_handlers/league/change_tpp.py
@@ -1,9 +1,7 @@
 
 from common_messages import invalid_number_of_params
-from helpers import can_be_int, valid_number_of_params
+from helpers import can_be_int, generic_find_user, valid_number_of_params
 from league import update_team_info, validate_admin
-from user import get_league_team, set_user_league_team, user_exists
-import constants
 
 
 async def change_tpp_handler(db, message, client):
@@ -11,10 +9,6 @@ async def change_tpp_handler(db, message, client):
     valid_params, params = valid_number_of_params(message, 3)
     if not valid_params:
         await invalid_number_of_params(message)
-        return
-    
-    if len(message.mentions) < 1:
-        await message.channel.send('Please mention the player to change their TPP.')
         return
     
     is_admin, my_team, team_name, _ = await validate_admin(db, message)
@@ -25,16 +19,20 @@ async def change_tpp_handler(db, message, client):
         await message.channel.send('You are not an admin of this league team. Please ask the team owner to be an admin.')
         return
     
-    member_to_find = message.mentions[0]
+    user_mentioned = params[1]
+    user_obj = generic_find_user(client, db, user_mentioned)
+    if not user_obj:
+        await message.channel.send('I could not find any user with that username/user id.')
+        return
+
     at_member = None
     at_member_index = 0
     cur_index = 0
-    old_tpp = 0
+
     for member in team_members:
-        if member['discord_id'] == member_to_find.id:
+        if member['discord_id'] == user_obj['discord_id']:
             at_member = member
             at_member_index = cur_index
-            old_tpp = member['TPP']
             break
         cur_index += 1
 
@@ -64,9 +62,6 @@ async def change_tpp_handler(db, message, client):
     my_team['members'][at_member_index]['TPP'] = tpp_offer
     league_teams = db['leagueteams']
     league_teams.update_one({'team_name': team_name}, {"$set": {"members": my_team['members']}})
-
-    league_notifs_channel = client.get_channel(constants.TEAM_NOTIFS_CHANNEL)
-    await league_notifs_channel.send('Team Update for '+team_name+": "+member_to_find.mention+"'s TPP has been updated from "+str(old_tpp)+' to '+str(tpp_offer))
 
     await update_team_info(client, my_team, db)
 
