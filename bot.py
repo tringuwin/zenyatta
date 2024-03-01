93fb65f4fb2e0a30080611f8497032b8f764c668
Logan Stanford <loganstanford53@gmail.com>
2024-03-01 16:09:54 -0500

    fart balls

--- bot.py
+++ bot.py
@@ -350,6 +350,9 @@ async def handle_message(message, db, client):
     elif lower_message.startswith('!whichhero'):
         await which_hero_handler(message)
 
+    elif lower_message == '!store':
+        await message.channel.send('Check out the official SOL Merch Store here! https://exclaim.gg/store/spicyow')
+
     elif lower_message.startswith('!whichteam'):
         await which_team_handler(message)
 
