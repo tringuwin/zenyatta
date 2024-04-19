89a53748af076b8d401de07ca27648b3c3aeeeeb
Logan Stanford <loganstanford53@gmail.com>
2024-04-19 00:06:19 -0400

    balls

--- poke_data.py
+++ poke_data.py
@@ -2,11 +2,59 @@
 
 SCARLET_AND_VIOLET_DATA = {
 
+    '8': {
+        'name': 'Basic Metal Energy',
+        'type': 'energy',
+        'card_img': 'https://i.postimg.cc/tT17MYSq/energy-metal.jpg'
+    },
+
+    '026': {
+        'name': 'Toedscruel',
+        'type': 'grass',
+        'card_img': 'https://i.postimg.cc/g2Kk0vXt/SV01-EN-26.png'
+    },
+
+    '044': {
+        'name': 'Magikarp',
+        'type': 'water',
+        'card_img': 'https://i.postimg.cc/x8hv4wy7/SV01-EN-44.png'
+    },
+
+    '058': {
+        'name': 'Cetoddle',
+        'type': 'water',
+        'card_img': 'https://i.postimg.cc/c4fXrc7S/SV01-EN-58.png'
+    },
+
+    '063': {
+        'name': 'Magnemite',
+        'type': 'electric',
+        'card_img': 'https://i.postimg.cc/Dz3h16HP/SV01-EN-63.png'
+    },
+
+    '090': {
+        'name': 'Drifblim',
+        'type': 'psychic',
+        'card_img': 'https://i.postimg.cc/RVBT5Bc1/SV01-EN-90.png'
+    }, 
+
     '169': {
         'name': 'Defiance Band',
         'type': 'trainer',
         'card_img': 'https://i.postimg.cc/4djHffCf/SV01-EN-169.png'
-    }
+    },
+
+    '173': {
+        'name': 'Energy Switch',
+        'type': 'trainer',
+        'card_img': 'https://i.postimg.cc/rF9ZfJD9/SV01-EN-173.png'
+    },
+
+    '178': {
+        'name': 'Mesagoza',
+        'type': 'trainer',
+        'card_img': 'https://i.postimg.cc/L8YfJ4FZ/SV01-EN-178.png'
+    },
 
 
 }
