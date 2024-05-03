2c6fc9cf9129fa154e4dc3c92feb8efe2fc143e6
Logan Stanford <loganstanford53@gmail.com>
2024-05-03 13:58:42 -0400

    nuts

--- command_handlers/league/update_team.py
+++ command_handlers/league/update_team.py
@@ -3,6 +3,8 @@ from discord_actions import get_guild
 from league import update_team_info
 import discord
 
+from user import user_exists
+
 
 async def update_team(db, team_name, client, message):
 
@@ -15,15 +17,24 @@ async def update_team(db, team_name, client, message):
         return
     
     final_team_members = []
+    ids_to_remove_team_from = []
     for member in team_object['members']:
         try:
             guild_member = await guild.fetch_member(member['discord_id'])
         except discord.NotFound:
             guild_member = None
+            ids_to_remove_team_from.append(member['discord_id'])
         if guild_member:
             final_team_members.append(member)
 
     league_teams.update_one({'team_name': team_name}, {"$set": {"members": final_team_members}})
+
+    users = db['users']
+    for user_id in ids_to_remove_team_from:
+        user = user_exists(db, user_id)
+        if user:
+            users.update_one({"discord_id": user_id}, {"$set": {"league_team": 'None'}})
+
 
     team_object['members'] = final_team_members
     await update_team_info(client, team_object, db)
