e7b13dde7d5f29a8487783f2f32252fa95b76ed0
Logan Stanford <loganstanford53@gmail.com>
2024-05-12 15:34:18 -0400

    baka

--- admin_handlers/total_league.py
+++ admin_handlers/total_league.py
@@ -5,7 +5,11 @@ async def total_league_handler(db, message):
     all_teams = leagueteams.find()
 
     total_players = 0
+    total_teams = 0
     for team in all_teams:
+        total_teams += 1
         total_players += len(team['members'])
 
-    await message.channel.send('Total players in SOL: '+str(total_players))
+    total_possible = int(total_teams * 25)
+
+    await message.channel.send('Total players in SOL: '+str(total_players)+'/'+str(total_possible))
