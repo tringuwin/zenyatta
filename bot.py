9e66d4c695d04ecb6e133b0997ec87b6fc782678
Logan Stanford <loganstanford53@gmail.com>
2024-05-13 00:09:49 -0400

    balls

--- bot.py
+++ bot.py
@@ -900,7 +900,7 @@ async def handle_message(message, db, client):
         
         reward_per_round = [25, 50, 100, 300, 1000, 2000, 2000, 2000]
 
-        bracket = db['brackets'].find_one({'event_id': '31'})
+        bracket = db['brackets'].find_one({'event_id': '32'})
 
         final_dict = {}
 
