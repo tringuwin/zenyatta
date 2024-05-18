5a92811e84078443ac396d5272b23d55d8730d39
Logan Stanford <loganstanford53@gmail.com>
2024-05-18 13:28:10 -0400

    sus

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ from command_handlers.league.league_invite import league_invite_handler
 from command_handlers.league.league_invites import league_invites_handler
 from command_handlers.league.league_kick import league_kick_handler
 from command_handlers.league.league_leave import league_leave_handler
-from command_handlers.league.league_xp import league_xp_handler
+from command_handlers.league.league_xp import league_xp_handler, total_league_xp_handler
 from command_handlers.league.lft.set_lft_color import set_lft_color_handler
 from command_handlers.league.lft.set_lft_hero import set_lft_hero_handler
 from command_handlers.league.lft.set_rank import set_rank_handler
@@ -591,6 +591,9 @@ async def handle_message(message, db, client):
 
     elif lower_message == '!leaguexp':
         await league_xp_handler(db, message, client)
+
+    elif lower_message == '!totalleaguexp':
+        await total_league_xp_handler(db, message, client)
 
     elif lower_message == '!pingteam':
         await ping_team_handler(db, message, client)
--- command_handlers/league/league_xp.py
+++ command_handlers/league/league_xp.py
@@ -1,14 +1,15 @@
 import constants
 
-async def league_xp_handler(db, message, client):
+
+async def general_league_xp(db, message, client, start_string, constant_name):
 
     constants_db = db['constants']
-    league_xp_obj = constants_db.find_one({'name': 'league_xp'})
+    league_xp_obj = constants_db.find_one({'name': constant_name})
     league_xp = league_xp_obj['value']
 
     sorted_list = sorted(league_xp.items(), key=lambda item: item[1], reverse=True)
 
-    final_string = '**LEAGUE XP STANDINGS:**'
+    final_string = start_string
 
     guild = client.get_guild(constants.GUILD_ID)
 
@@ -22,3 +23,14 @@ async def league_xp_handler(db, message, client):
         index += 1
 
     await message.channel.send(final_string)
+
+
+
+async def league_xp_handler(db, message, client):
+
+    await general_league_xp(db, message, client, '**MONTHLY LEAGUE XP STANDINGS:**', 'league_xp')
+
+
+async def total_league_xp_handler(db, message, client):
+
+    await general_league_xp(db, message, client, '**ALL-TIME LEAGUE XP STANDINGS:**', 'league_xp_total')
