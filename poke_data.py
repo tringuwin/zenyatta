18ac1358553488f471190c7f6d901b68a82fabd1
Logan Stanford <loganstanford53@gmail.com>
2024-05-19 14:01:03 -0400

    sus

--- poke_data.py
+++ poke_data.py
@@ -5,25 +5,25 @@ SCARLET_AND_VIOLET_DATA = {
     '2': {
         'name': 'Basic Fire Energy',
         'type': 'energy',
-        'card_img': 'https://i.postimg.cc/RFqZ84Xc/energy-fire.jpg'
+        'card_img': 'https://i.imgur.com/y5rjm8y.jpeg'
     },
 
     '4': {
         'name': 'Basic Electric Energy',
         'type': 'energy',
-        'card_img': 'https://i.postimg.cc/Cxmdm8dd/energy-electric.jpg'
+        'card_img': 'https://i.imgur.com/Zzf1yCk.jpeg'
     },
 
     '8': {
         'name': 'Basic Metal Energy',
         'type': 'energy',
-        'card_img': 'https://i.postimg.cc/tT17MYSq/energy-metal.jpg'
+        'card_img': 'https://i.imgur.com/ac2PPlu.jpeg'
     },
 
     '026': {
         'name': 'Toedscruel',
         'type': 'grass',
-        'card_img': 'https://i.postimg.cc/g2Kk0vXt/SV01-EN-26.png'
+        'card_img': 'https://i.imgur.com/q6tHTP7.png'
     },
 
     '042': {
@@ -35,7 +35,7 @@ SCARLET_AND_VIOLET_DATA = {
     '044': {
         'name': 'Magikarp',
         'type': 'water',
-        'card_img': 'https://i.postimg.cc/x8hv4wy7/SV01-EN-44.png'
+        'card_img': 'https://i.imgur.com/Jly2no9.png'
     },
 
     '049': {
@@ -47,13 +47,13 @@ SCARLET_AND_VIOLET_DATA = {
     '058': {
         'name': 'Cetoddle',
         'type': 'water',
-        'card_img': 'https://i.postimg.cc/c4fXrc7S/SV01-EN-58.png'
+        'card_img': 'https://i.imgur.com/BbhO4Nj.png'
     },
 
     '063': {
         'name': 'Magnemite',
         'type': 'electric',
-        'card_img': 'https://i.postimg.cc/Dz3h16HP/SV01-EN-63.png'
+        'card_img': 'https://i.imgur.com/5dKKXoa.png'
     },
 
     '068': {
@@ -77,13 +77,13 @@ SCARLET_AND_VIOLET_DATA = {
     '090': {
         'name': 'Drifblim',
         'type': 'psychic',
-        'card_img': 'https://i.postimg.cc/RVBT5Bc1/SV01-EN-90.png'
+        'card_img': 'https://i.imgur.com/h1YKn6B.png'
     }, 
 
     '123': {
         'name': 'Great Tusk',
         'type': 'fighting',
-        'card_img': 'https://i.postimg.cc/Hn7FchkG/SV01-EN-123.png'
+        'card_img': 'https://i.imgur.com/4bUsrjL.png'
     },
 
     '134': {
@@ -95,25 +95,25 @@ SCARLET_AND_VIOLET_DATA = {
     '140': {
         'name': 'Varoom',
         'type': 'metal',
-        'card_img': 'https://i.postimg.cc/Z5RK76jW/SV01-EN-140.png'
+        'card_img': 'https://i.imgur.com/o6ou61A.png'
     },
 
     '169': {
         'name': 'Defiance Band',
         'type': 'trainer',
-        'card_img': 'https://i.postimg.cc/4djHffCf/SV01-EN-169.png'
+        'card_img': 'https://i.imgur.com/Z1ZDLXh.png'
     },
 
     '173': {
         'name': 'Energy Switch',
         'type': 'trainer',
-        'card_img': 'https://i.postimg.cc/rF9ZfJD9/SV01-EN-173.png'
+        'card_img': 'https://i.imgur.com/r3Qa57d.png'
     },
 
     '178': {
         'name': 'Mesagoza',
         'type': 'trainer',
-        'card_img': 'https://i.postimg.cc/L8YfJ4FZ/SV01-EN-178.png'
+        'card_img': 'https://i.imgur.com/GjvhTKQ.png'
     },
 
 
