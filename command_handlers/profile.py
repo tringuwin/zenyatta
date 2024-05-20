8a6822cf0f59563d5115d2e52c356d0e6663db4e
Logan Stanford <loganstanford53@gmail.com>
2024-05-20 17:21:34 -0400

    profile

--- command_handlers/profile.py
+++ command_handlers/profile.py
@@ -1,4 +1,5 @@
 
+from common_messages import not_registered_response
 from discord_actions import get_guild, get_member_by_username
 from helpers import generic_find_user, make_string_from_word_list
 from user import get_fan_of, get_league_team, get_lvl_info, get_rival_of, get_twitch_username, get_user_gems, get_user_packs, get_user_passes, get_user_pickaxes, get_user_poke_points, get_user_ranks, get_user_tokens, user_exists
@@ -74,12 +75,19 @@ async def profile_handler(db, message, client):
 
     word_list = message.content.split()
     user = None
+    not_reg = False
     if len(word_list) == 1:
         user = user_exists(db, message.author.id)
+        if not user:
+            not_reg = True
     else:
         username = make_string_from_word_list(word_list, 1)
         user = await generic_find_user(client, db, username)
     
+    if not_reg:
+        await not_registered_response(message)
+        return
+
     if not user:
         await message.channel.send('User not found.')
         return
