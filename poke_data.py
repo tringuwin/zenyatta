eb8cd3ce544dc44882d855abadf797c6eb628230
Logan Stanford <loganstanford53@gmail.com>
2024-05-20 17:06:48 -0400

    nutsack

--- poke_data.py
+++ poke_data.py
@@ -536,16 +536,316 @@ SCARLET_AND_VIOLET_DATA = {
         'card_img': 'https://i.imgur.com/iCuhmP3.png'
     },
 
+    '082': {
+        'name': 'Drowzee',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/dsYtQBx.png'
+    },
+
+    '083': {
+        'name': 'Hypno',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/jFhmP3i.png'
+    },
+
+    '084': {
+        'name': 'Ralts',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/MvEJiHS.png'
+    },
+
+    '085': {
+        'name': 'Kirlia',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/86XfqtI.png'
+    },
+
+    '086': {
+        'name': 'Gardevoir',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/84lNaHl.png'
+    },
+
+    '087': {
+        'name': 'Shuppet',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/RoE7Zbn.png'
+    },
+
+    '088': {
+        'name': 'Banette',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/m5pyptG.png'
+    },
+
+    '089': {
+        'name': 'Drifloon',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/Wlg4F2T.png'
+    },
+
     '090': {
         'name': 'Drifblim',
         'type': 'psychic',
         'card_img': 'https://i.imgur.com/h1YKn6B.png'
+    },
+
+    '091': {
+        'name': 'Flabebe',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/hiJz49N.png'
+    },
+
+    '092': {
+        'name': 'Floette',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/S7ZvSNJ.png'
+    },
+
+    '093': {
+        'name': 'Florges',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/Y23WT7L.png'
+    },
+
+    '094': {
+        'name': 'Dedenne',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/rirQVTL.png'
+    },
+
+    '095': {
+        'name': 'Dedenne',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/TRTq6MT.png'
+    },
+
+    '096': {
+        'name': 'Klefki',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/BaVyTph.png'
+    },
+
+    '097': {
+        'name': 'Fidough',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/KCX5M2c.png'
+    },
+
+    '098': {
+        'name': 'Fidough',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/qZq66ZE.png'
+    },
+
+    '099': {
+        'name': 'Dachsbun',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/jWYwj9W.png'
     }, 
+
+    '100': {
+        'name': 'Flittle',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/UqgPBHH.png'
+    },
+
+    '101': {
+        'name': 'Flittle',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/rFThjNb.png'
+    },
+
+    '102': {
+        'name': 'Flittle',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/5eQmexP.png'
+    },
+
+    '103': {
+        'name': 'Espathra',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/EoVztBq.png'
+    },
+
+    '104': {
+        'name': 'Greavard',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/YNwQNjZ.png'
+    },
+
+    '105': {
+        'name': 'Greavard',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/lRYdFgd.png'
+    },
+
+    '106': {
+        'name': 'Houndstone',
+        'type': 'psychic',
+        'card_img': 'https://i.imgur.com/YWkBnvb.png'
+    },
+
+    '107': {
+        'name': 'Mankey',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/NSVEebi.png'
+    },
+
+    '108': {
+        'name': 'Primeape',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/hRKdH0A.png'
+    },
+
+    '109': {
+        'name': 'Annihilape',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/tkcQF1u.png'
+    },
+
+    '110': {
+        'name': 'Meditite',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/0NKPpMA.png'
+    },
+
+    '111': {
+        'name': 'Medicham',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/B7RF9JS.png'
+    },
+
+    '112': {
+        'name': 'Riolu',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/3Hx1gGA.png'
+    },
+
+    '113': {
+        'name': 'Riolu',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/qn9ZOvy.png'
+    },
+
+    '114': {
+        'name': 'Lucario',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/H13TlS5.png'
+    },
+
+    '115': {
+        'name': 'Sandile',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/fTM42Rt.png'
+    },
+
+    '116': {
+        'name': 'Krokorok',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/OtxPTjv.png'
+    },
+
+    '117': {
+        'name': 'Krookodile',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/HNCYCeX.png'
+    },
+
+    '118': {
+        'name': 'Hawlucha',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/K65YKNt.png'
+    },
+
+    '119': {
+        'name': 'Silicobra',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/yfbLg18.png'
+    },
+
+    '120': {
+        'name': 'Sandaconda',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/1zTjktY.png'
+    },
+
+    '124': {
+        'name': 'Stonjourner',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/oiAKbEv.png'
+    },
+
+    '122': {
+        'name': 'Klawf',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/fn2liYc.png'
+    },
 
     '123': {
         'name': 'Great Tusk',
         'type': 'fighting',
         'card_img': 'https://i.imgur.com/4bUsrjL.png'
+    },
+
+    '124': {
+        'name': 'Koraidon',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/Cbhazbp.png'
+    },
+
+    '125': {
+        'name': 'Koraidon',
+        'type': 'fighting',
+        'card_img': 'https://i.imgur.com/QSC7y2r.png'
+    },
+
+    '126': {
+        'name': 'Grimer',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/kur7UfZ.png'
+    },
+
+    '127': {
+        'name': 'Muk',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/PXd3FaG.png'
+    },
+
+    '128': {
+        'name': 'Seviper',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/DHfFVcD.png'
+    },
+
+    '129': {
+        'name': 'Spiritomb',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/32UsytC.png'
+    },
+
+    '130': {
+        'name': 'Croagunk',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/JImDzJ4.png'
+    },
+
+    '131': {
+        'name': 'Toxicroak',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/xPF38TM.png'
+    },
+
+    '132': {
+        'name': 'Pawniard',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/jBWMWRr.png'
+    },
+
+    '133': {
+        'name': 'Bisharp',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/Fqdhb0T.png'
     },
 
     '134': {
@@ -554,10 +854,208 @@ SCARLET_AND_VIOLET_DATA = {
         'card_img': 'https://i.imgur.com/33ubWXt.png'
     },
 
+    '135': {
+        'name': 'Maschiff',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/3C4apkU.png'
+    },
+
+    '136': {
+        'name': 'Maschiff',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/JzabVBY.png'
+    },
+
+    '137': {
+        'name': 'Mabosstiff',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/74b5yrf.png'
+    },
+
+    '138': {
+        'name': 'Bombirdier',
+        'type': 'darkness',
+        'card_img': 'https://i.imgur.com/8xLaXur.png'
+    },
+
+    '139': {
+        'name': 'Forretress',
+        'type': 'metal',
+        'card_img': 'blob:https://imgur.com/4d8c81ba-c346-44cd-869a-5c7c8ac00783'
+    },
+
     '140': {
         'name': 'Varoom',
         'type': 'metal',
         'card_img': 'https://i.imgur.com/o6ou61A.png'
+    },
+
+    '141': {
+        'name': 'Varoom',
+        'type': 'metal',
+        'card_img': 'https://i.imgur.com/ezBcfqe.png'
+    },
+
+    '142': {
+        'name': 'Revaroom',
+        'type': 'metal',
+        'card_img': 'https://i.imgur.com/OI4ESFt.png'
+    },
+
+    '143': {
+        'name': 'Iron Treads',
+        'type': 'metal',
+        'card_img': 'https://i.imgur.com/TsFNBSW.png'
+    },
+
+    '144': {
+        'name': 'Chansey',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/MpnBurq.png'
+    },
+
+    '145': {
+        'name': 'Blissey',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/2xDSnV8.png'
+    },
+
+    '146': {
+        'name': 'Zangoose',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/NRY2TMh.png'
+    },
+
+    '147': {
+        'name': 'Zangoose',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/Glqq7al.png'
+    },
+
+    '148': {
+        'name': 'Starly',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/ltWroOE.png'
+    },
+
+    '149': {
+        'name': 'Staravia',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/pIh2VAn.png'
+    },
+
+    '150': {
+        'name': 'Staraptor',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/r3Dyw35.png'
+    },
+
+    '151': {
+        'name': 'Skwovet',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/JCs6qYY.png'
+    },
+
+    '152': {
+        'name': 'Greedent',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/hnrC8CB.png'
+    },
+
+    '153': {
+        'name': 'Indeedee',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/C1XAbeQ.png'
+    },
+
+    '154': {
+        'name': 'Lechonk',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/WUQjP1v.png'
+    },
+
+    '155': {
+        'name': 'Lechonk',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/QpdMQYJ.png'
+    },
+
+    '156': {
+        'name': 'Lechonk',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/dpPNvej.png'
+    },
+
+    '157': {
+        'name': 'Oinkologne',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/602Q4TS.png'
+    },
+
+    '158': {
+        'name': 'Oinkologne',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/1mqLfmu.png'
+    },
+
+    '159': {
+        'name': 'Tandemaus',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/m7FSSnF.png'
+    },
+
+    '160': {
+        'name': 'Tandemaus',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/3eAk4dy.png'
+    },
+
+    '161': {
+        'name': 'Maushold',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/xk3Fg4T.png'
+    },
+
+    '162': {
+        'name': 'Maushold',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/xk3Fg4T.png'
+    },
+
+    '163': {
+        'name': 'Squawkabilly',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/WHOrJwd.png'
+    },
+
+    '164': {
+        'name': 'Cyclizar',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/YMGYELo.png'
+    },
+
+    '165': {
+        'name': 'Cyclizar',
+        'type': 'normal',
+        'card_img': 'https://i.imgur.com/M5hWtA6.png'
+    },
+
+    '166': {
+        'name': 'Arven',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/Dl8Iq9w.png'
+    },
+
+    '167': {
+        'name': 'Beach Court',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/SnQCK7J.png'
+    },
+
+    '168': {
+        'name': 'Crushing Hammer',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/9D5aT33.png'
     },
 
     '169': {
@@ -566,16 +1064,178 @@ SCARLET_AND_VIOLET_DATA = {
         'card_img': 'https://i.imgur.com/Z1ZDLXh.png'
     },
 
+    '170': {
+        'name': 'Electric Generator',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/F32dzq5.png'
+    },
+
+    '171': {
+        'name': 'Energy Retrieval',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/aM14bj3.png'
+    },
+
+    '172': {
+        'name': 'Energy Search',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/LaJZzvW.png'
+    },
+
     '173': {
         'name': 'Energy Switch',
         'type': 'trainer',
         'card_img': 'https://i.imgur.com/r3Qa57d.png'
     },
 
+    '174': {
+        'name': 'Exp. Share',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/NKQlaPo.png'
+    },
+
+    '175': {
+        'name': 'Jacq',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/NjrIspi.png'
+    },
+
+    '176': {
+        'name': 'Judge',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/SZwnjGY.png'
+    },
+
+    '177': {
+        'name': 'Katy',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/AeLiX0r.png'
+    },
+
     '178': {
         'name': 'Mesagoza',
         'type': 'trainer',
         'card_img': 'https://i.imgur.com/GjvhTKQ.png'
+    },
+
+    '179': {
+        'name': 'Miriam',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/8QfPTOo.png'
+    },
+
+    '180': {
+        'name': 'Nemona',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/UliwEDB.png'
+    },
+
+    '181': {
+        'name': 'Nest Ball',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/RB3SCh3.png'
+    },
+
+    '182': {
+        'name': 'Pal Pad',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/dA1kLbT.png'
+    },
+
+    '183': {
+        'name': 'Penny',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/rFKVhwD.png'
+    },
+
+    '184': {
+        'name': 'Picnic Basket',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/Vaob80O.png'
+    },
+
+    '185': {
+        'name': 'Poke Ball',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/M21ipjH.png'
+    },
+
+    '186': {
+        'name': 'Pokegear 3.0',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/IuL3pnv.png'
+    },
+
+    '187': {
+        'name': 'Pokemon Catcher',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/Sf3dREh.png'
+    },
+
+    '188': {
+        'name': 'Potion',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/KynKbLK.png'
+    },
+
+    '189': {
+        'name': "Professor's Research",
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/jdtZcpf.png'
+    },
+
+    '190': {
+        'name': "Professor's Research",
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/MRVE8iE.png'
+    },
+
+    '191': {
+        'name': 'Rare Candy',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/VBYHXlD.png'
+    },
+
+    '192': {
+        'name': 'Rock Chestplate',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/evgOshD.png'
+    },
+
+    '193': {
+        'name': 'Rocky Helmet',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/I0ZJUEF.png'
+    },
+
+    '194': {
+        'name': 'Switch',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/GHKESxj.png'
+    },
+
+    '195': {
+        'name': 'Team Star Grunt',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/5rS6Dwz.png'
+    },
+
+    '196': {
+        'name': 'Ultra Ball',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/SrpyB25.png'
+    },
+
+    '197': {
+        'name': 'Vitality Band',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/RDMBJwP.png'
+    },
+
+    '198': {
+        'name': 'Youngster',
+        'type': 'trainer',
+        'card_img': 'https://i.imgur.com/CEqLewn.png'
     },
 
 
