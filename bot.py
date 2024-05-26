1f1dea687cc9146cab4ba1d66746e9092abf9439
Logan Stanford <loganstanford53@gmail.com>
2024-05-26 02:03:24 -0400

    sus

--- bot.py
+++ bot.py
@@ -811,6 +811,9 @@ async def handle_message(message, db, client):
     elif lower_message == '!fortnite':
         await message.channel.send('fortnite')
 
+    elif lower_message == '!thepoint':
+        await message.channel.send('https://i.imgur.com/mwekfl2.png')
+
     elif lower_message == '!testdm' and is_admin:
 
         guild = await get_guild(client)
