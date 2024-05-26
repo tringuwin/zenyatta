edc60f6905bcb0dedcaf356e43c7fafd57b2b91a
Logan Stanford <loganstanford53@gmail.com>
2024-05-26 02:24:08 -0400

    buns

--- bot.py
+++ bot.py
@@ -836,6 +836,28 @@ async def handle_message(message, db, client):
 
         await message.channel.send(default_msg)
 
+    elif lower_message.startswith('!8ball'):
+
+        eight_ball = [
+            'It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes definitely',
+            'You may rely on it',
+            'Reply hazy, try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            "Don't count on it",
+            'My reply is no',
+            'My sources say no',
+            'Outlook not so good',
+            'Very doubtful'
+        ]
+
+        await message.channel.send(random.choice(eight_ball))
+
     elif lower_message.startswith('!initraffleconst') and is_admin:
         
         db_constants = db['constants']
