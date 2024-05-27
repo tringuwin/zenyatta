aaf0b1bdf8db9d977f59f5ffe0ec87abd1e65942
Logan Stanford <loganstanford53@gmail.com>
2024-05-27 02:37:32 -0400

    sus

--- bot.py
+++ bot.py
@@ -180,7 +180,7 @@ def is_valid_channel(message, lower_message, is_helper, is_push_bot):
     if is_helper or is_push_bot:
         return True, None
     
-    if lower_message == '!hello' or lower_message == '!gg ez' or lower_message.startswith('!whichteam') or lower_message.startswith('!whichhero') or lower_message=='!pingteam' or lower_message.startswith('!profile') or lower_message.startswith('!bandforband'):
+    if lower_message == '!hello' or lower_message == '!gg ez' or lower_message.startswith('!whichteam') or lower_message.startswith('!whichhero') or lower_message=='!pingteam' or lower_message.startswith('!profile') or lower_message.startswith('!bandforband') or lower_message == '!fortnite':
         return True, None
 
     if message.channel.id == constants.BOT_CHANNEL:
