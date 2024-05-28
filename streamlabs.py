7db5576a933c43293b2edcf4f22351890af361ea
Logan Stanford <loganstanford53@gmail.com>
2024-05-28 15:47:54 -0400

    fortnite ass

--- streamlabs.py
+++ streamlabs.py
@@ -1,6 +1,9 @@
 import requests
 from datetime import datetime
 
+from rewards import change_tokens
+from user import twitch_user_exists
+
 stream_labs_data = 'https://streamlabs.com/api/v5/giveaway/history?token=B032D12F02A4ED3AA822&page=1'
 
 
@@ -41,6 +44,40 @@ async def check_streamlabs_raffles(db, channel):
 
         if redeem_date > last_date:
             valid_date_redeems.append(redeem)
+
+    if len(valid_date_redeems) == 0:
+        return
+
+    most_recent_date_raw = None
+    most_recent_date = None
+
+    for valid_redeem in valid_date_redeems:
+        
+        redeem_date_raw = valid_redeem['updated_at']
+        redeem_date = datetime.strptime(redeem_date_raw, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+        if (not most_recent_date ) or redeem_date > most_recent_date:
+            most_recent_date_raw = redeem_date_raw
+            most_recent_date = redeem_date
+
+        winner_twitch = valid_redeem['winners'][0]['name']
+        user = twitch_user_exists(db, winner_twitch)
+        if not user:
+            continue
+
+        prize_name = valid_redeem['settings']['general']['name']
+
+        # handle give prize
+        if prize_name == '500 Tokens':
+            print('Giving 500 tokens to '+winner_twitch)
+            #change_tokens(db, user, 500)
+        elif prize_name == 'SOL Card Pack':
+            print('Giving a pack to '+winner_twitch)
+        
+        elif prize_name == 'Random Gem':
+            print('Giving random gem to '+winner_twitch)
+
+            
 
     print('Total number of valid date: '+str(len(valid_date_redeems)))
 
