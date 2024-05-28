2499915406118c96eee3ee1a42f0812d809b2547
Logan Stanford <loganstanford53@gmail.com>
2024-05-28 17:10:39 -0400

    sus

--- bot.py
+++ bot.py
@@ -1038,7 +1038,7 @@ async def handle_message(message, db, client):
         
         reward_per_round = [25, 50, 100, 300, 1000, 2000, 2000, 2000]
 
-        bracket = db['brackets'].find_one({'event_id': '32'})
+        bracket = db['brackets'].find_one({'event_id': '34'})
 
         final_dict = {}
 
