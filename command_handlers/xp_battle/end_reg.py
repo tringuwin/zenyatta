b7e3f2b96faf774e0f9553d4278e4c0022a46419
Logan Stanford <loganstanford53@gmail.com>
2024-06-03 18:54:35 -0400

    susery baka

--- command_handlers/xp_battle/end_reg.py
+++ command_handlers/xp_battle/end_reg.py
@@ -1,13 +1,12 @@
 
 
+from helpers import get_constant_value, set_constant_value
 from xp_battles import init_player_pools
 
 
 async def end_reg_handler(db, message):
 
-    constants_db = db['constants']
-    battle_obj = constants_db.find_one({'name': 'battle'})
-    battle_info = battle_obj['value']
+    battle_info = get_constant_value(db, 'battle')
 
     number_sign_ups = len(battle_info['sign_ups'])
     if number_sign_ups < 9:
@@ -21,5 +20,6 @@ async def end_reg_handler(db, message):
 
     current_players = []
     
+    set_constant_value(db, 'battle', battle_info)
 
     await message.channel.send('Registration ended')
