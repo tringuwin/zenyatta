68d42e83723a3edaaabcfccce39f9cc75812f515
Logan Stanford <loganstanford53@gmail.com>
2024-06-02 21:59:49 -0400

    fucl

--- admin_handlers/sub_rewards.py
+++ admin_handlers/sub_rewards.py
@@ -1,6 +1,6 @@
 
 
-from helpers import generic_find_user, valid_number_of_params
+from helpers import can_be_int, generic_find_user, valid_number_of_params
 from rewards import change_packs, change_pp
 from user import get_sub_lootboxes
 
@@ -27,3 +27,33 @@ async def sub_rewards_handler(client, db, message):
     users.update_one({"discord_id": user['discord_id']}, {"$set": {"sub_lootboxes": sub_boxes + 1}})
 
     await message.channel.send('Gave user 3 Packs, 300 XP and a sub lootbox to the user.')
+
+
+async def gift_rewards_handler(client, db, message):
+
+    valid_params, params = valid_number_of_params(message, 3)
+    if not valid_params:
+        await message.channel.send('Invalid number of params')
+        return
+    
+    user_id = params[1]
+
+    user = await generic_find_user(client, db, user_id)
+    if not user:
+        await message.channel.send('Could not find a matching user.')
+        return
+    
+    num_to_give = params[2]
+    if not can_be_int(num_to_give):
+        await message.channel.send(num_to_give+' is not a number')
+        return
+    
+    num_to_give = int(num_to_give)
+    if num_to_give < 1:
+        await message.channel.send('Number of gifted rewards to give must be greater than 0')
+        return
+    
+    await change_packs(db, user, int(3 * num_to_give))
+    await change_pp(db, user, int(300 * num_to_give))
+
+    await message.channel.send('Gave gifted sub rewards to the user.')
