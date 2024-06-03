06db58e05e2cf9b2933179058e3cb053ed827555
Logan Stanford <loganstanford53@gmail.com>
2024-06-03 19:09:18 -0400

    chungass

--- command_handlers/xp_battle/end_reg.py
+++ command_handlers/xp_battle/end_reg.py
@@ -2,7 +2,7 @@
 
 from helpers import get_constant_value, set_constant_value
 from xp_battles import init_player_pools
-
+import random
 
 async def end_reg_handler(db, message):
 
@@ -18,8 +18,19 @@ async def end_reg_handler(db, message):
     battle_info['player_pools'] = player_pools
     battle_info['reg_open'] = False
 
+    valid_players = battle_info['player_pools']['valid_pool'] 
     current_players = []
-    
+
+    while len(valid_players) > 0 and len(current_players) < 9:
+        index = random.randint(0, len(valid_players) - 1) 
+        removed_item = valid_players.pop(index)
+        current_players.append(removed_item)  
+
+
+    # while len(current_players) < 9:
+
+    battle_info['current_players'] = current_players
+
     set_constant_value(db, 'battle', battle_info)
 
     await message.channel.send('Registration ended')
