35ab5d7fb71745ddf3727a6de39613eda2be834d
Logan Stanford <loganstanford53@gmail.com>
2024-06-03 20:13:09 -0400

    sus

--- command_handlers/xp_battle/end_battle.py
+++ command_handlers/xp_battle/end_battle.py
@@ -10,7 +10,13 @@ async def end_battle_handler(db, message):
     battle_info['battle_on'] = False
     battle_info['reg_open'] = False
 
-    battle_info['past_players'].append(battle_info['current_players'])
+    players_in_battle = []
+    for team_name in battle_info['current_teams']:
+        team = battle_info['current_teams'][team_name]
+        for user_id in team:
+            players_in_battle.append(user_id)
+
+    battle_info['past_players'].append(players_in_battle)
 
     set_constant_value(db, 'battle', battle_info)
 
