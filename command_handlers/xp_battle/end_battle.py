e3650f1491790a61afce047d462297812a5ded0b
Logan Stanford <loganstanford53@gmail.com>
2024-06-03 20:09:25 -0400

    blud

--- command_handlers/xp_battle/end_battle.py
+++ command_handlers/xp_battle/end_battle.py
@@ -10,6 +10,8 @@ async def end_battle_handler(db, message):
     battle_info['battle_on'] = False
     battle_info['reg_open'] = False
 
+    battle_info['past_players'].append(battle_info['current_players'])
+
     set_constant_value(db, 'battle', battle_info)
 
     await message.channel.send('Battle ended.')
--- command_handlers/xp_battle/end_reg.py
+++ command_handlers/xp_battle/end_reg.py
@@ -22,22 +22,29 @@ async def end_reg_handler(db, message):
     if number_sign_ups < 9:
         await message.channel.send('There are only '+str(number_sign_ups)+' players signed up for the battle. We need at least 9 players to start.')
         return
-    
-    player_pools = init_player_pools(battle_info)
 
-    battle_info['player_pools'] = player_pools
     battle_info['reg_open'] = False
-
-    valid_players = battle_info['player_pools']['valid_pool'] 
     current_players = []
 
-    while len(valid_players) > 0 and len(current_players) < 9:
-        index = random.randint(0, len(valid_players) - 1) 
-        removed_item = valid_players.pop(index)
-        current_players.append(removed_item)  
+    while len(current_players) < 9:
+
+        battle_info['player_pools'] = init_player_pools(battle_info)
+
+        valid_players = battle_info['player_pools']['valid_pool'] 
+
+        while len(valid_players) > 0 and len(current_players) < 9:
+            index = random.randint(0, len(valid_players) - 1) 
+            removed_item = valid_players.pop(index)
+            battle_info['sign_ups'].remove(removed_item)
+            current_players.append(removed_item)  
+
+        if len(current_players) < 9:
+
+            past_players = battle_info['past_players']
+            past_players.pop(0)
+            battle_info['past_players'] = past_players
 
 
-    # while len(current_players) < 9:
 
     battle_info['current_players'] = current_players
     battle_info['player_pools']['valid_pool'] = valid_players
