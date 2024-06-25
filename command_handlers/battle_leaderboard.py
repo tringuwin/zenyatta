41ed4cce1270a025af873d2d5b6e924805083067
Logan Stanford <loganstanford53@gmail.com>
2024-06-25 01:05:41 -0400

    fartnite

--- command_handlers/battle_leaderboard.py
+++ command_handlers/battle_leaderboard.py
@@ -26,8 +26,9 @@ async def battle_leaderboard_handler(db, message):
     final_string = '**XP BATTLE LEADERBOARD**'
 
     for x in range(10):
+        print(x)
         rank_user = final_users[x]
-        final_string += '\n'+str(x)+'. '+rank_user['battle_tag']+' %'+str(rank_user['percent'])+' | W: '+str(rank_user['w'])+' | L: '+str(rank_user['l'])+' | T: '+str(rank_user['t'])
+        final_string += '\n'+str(x)+'. **'+rank_user['battle_tag']+'** | %'+str(rank_user['percent'])+' | W: '+str(rank_user['w'])+' | L: '+str(rank_user['l'])+' | T: '+str(rank_user['t'])
 
     await message.channel.send(final_string)
 
