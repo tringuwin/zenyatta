64fe2b0e64d4211cbf5220a7ec5973c247460242
Logan Stanford <loganstanford53@gmail.com>
2024-07-09 19:35:50 -0400

    ass

--- command_handlers/help/help_poke.py
+++ command_handlers/help/help_poke.py
@@ -6,6 +6,7 @@ async def help_poke_handler(message):
     help_embed.add_field(name='!openpoke', value='Spend 100 PokePoints to open earn a real pokemon card!', inline=False)
     help_embed.add_field(name='!sellpoke [card id]', value='Sell a pokemon card for 20 Tokens', inline=False)
     help_embed.add_field(name='!viewpoke [card id]', value='View a pokemon card', inline=False)
+    help_embed.add_field(name='!givepoke @user [card id]', value='Give one of your pokemon cards to another user', inline=False)
     help_embed.add_field(name='!allpokes', value='Get a link to a webpage that shows all of your Pokemon cards', inline=False)
     help_embed.add_field(name='!mypokes', value='See a list of pokemon card IDs that you own', inline=False)
     help_embed.add_field(name='!unopened', value='Get a link to a webpage that shows all Pokemon cards that have not been opened yet', inline=False)
