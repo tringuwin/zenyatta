1506640feaecdfa284d5bcc98b8cb34341b9a6d1
Logan Stanford <loganstanford53@gmail.com>
2024-07-12 19:12:39 -0400

    nuts

--- bot.py
+++ bot.py
@@ -372,7 +372,7 @@ async def handle_message(message, db, client):
         await help_poke_handler(message)
 
     elif lower_message.startswith('!address'):
-        notice = await message.channel.send('Please only use this command in a direct message with me for your safety!')
+        notice = await message.channel.send(message.author.mention+' Please only use this command in a direct message with me for your safety!')
         await message.delete()
         time.sleep(5)
         await notice.delete()
