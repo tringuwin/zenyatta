de72f33c06e26ba4f470f341736e7fa5e9f5d313
Logan Stanford <loganstanford53@gmail.com>
2024-07-27 18:35:16 -0400

    ass

--- command_handlers/help/help_lft.py
+++ command_handlers/help/help_lft.py
@@ -9,5 +9,6 @@ async def help_lft_handler(message):
     help_embed.add_field(name='!updateLFT', value="Updates your LFT profile if it's showing old data.", inline=False)
     help_embed.add_field(name='!setLFTcolor [hex code]', value="Set the background color for your LFT profile.", inline=False)
     help_embed.add_field(name='!setLFTHero [number 1-4] [hero name]', value="Set the heroes you play on your profile. There are 4 spots available.", inline=False)
+    help_embed.add_field(name='!bumpLFT', value="Make your profile appear at the top of the list for a higher chance of being noticed!", inline=False)
 
     await message.channel.send(embed=help_embed)
