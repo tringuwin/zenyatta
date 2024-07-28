9a1e4c3c8a7602555f5199400f5f46d32bfefad6
Logan Stanford <loganstanford53@gmail.com>
2024-07-28 19:12:21 -0400

    bums

--- command_handlers/league/ally/del_rival.py
+++ command_handlers/league/ally/del_rival.py
@@ -1,12 +1,10 @@
-
-
 from common_messages import invalid_number_of_params
 from discord_actions import get_guild
 from helpers import valid_number_of_params
 from league import validate_admin
 import constants
 
-async def del_ally_handler(db, message, client):
+async def del_rival_handler(db, message, client):
 
     valid_admin, _, team_name, _ = await validate_admin(db, message)
 
@@ -33,18 +31,18 @@ async def del_ally_handler(db, message, client):
         await message.channel.send('I did not find any league teams with that name... Check the spelling of the team name.')
         return
 
-    # is current ally
-    if not (other_team_obj['team_name'] in my_team_obj['allies']):
-        await message.channel.send('This team is not an Ally of your team.')
+    # is current rival
+    if not (other_team_obj['team_name'] in my_team_obj['rivals']):
+        await message.channel.send('This team is not a Rival of your team.')
         return
 
-    # remove ally from my team
-    my_team_obj['allies'].remove(other_team_obj['team_name'])
-    league_teams.update_one({'team_name': team_name}, {'$set': {'allies': my_team_obj['allies']}})
+    # remove rival from my team
+    my_team_obj['rivals'].remove(other_team_obj['team_name'])
+    league_teams.update_one({'team_name': team_name}, {'$set': {'rivals': my_team_obj['rivals']}})
 
-    # remove ally from other team
-    other_team_obj['allies'].remove(team_name)
-    league_teams.update_one({'team_name': other_team_obj['team_name']}, {'$set': {'allies': other_team_obj['allies']}})
+    # remove rival from other team
+    other_team_obj['rivals'].remove(team_name)
+    league_teams.update_one({'team_name': other_team_obj['team_name']}, {'$set': {'rivals': other_team_obj['rivals']}})
 
     # league notifs message
     league_notifs_channel = client.get_channel(constants.TEAM_NOTIFS_CHANNEL)
@@ -55,7 +53,7 @@ async def del_ally_handler(db, message, client):
     other_team_emoji_id = constants.LEAGUE_TO_EMOJI_ID[other_team_obj['team_name']]
     other_team_emoji = guild.get_emoji(other_team_emoji_id)
     
-    await league_notifs_channel.send(str(my_team_emoji)+' '+team_name+' and '+str(other_team_emoji)+' '+other_team_obj['team_name']+' are no longer Allies.')
+    await league_notifs_channel.send(str(my_team_emoji)+' '+team_name+' and '+str(other_team_emoji)+' '+other_team_obj['team_name']+' are no longer Rivals.')
 
     # confirmation message
-    await message.channel.send(team_name+' and '+other_team_obj['team_name']+' are no longer Allies.')
+    await message.channel.send(team_name+' and '+other_team_obj['team_name']+' are no longer Rivals.')
