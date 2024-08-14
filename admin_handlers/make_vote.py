2914a4944f297e1fc4d078184db19dce7021a27a
Logan Stanford <loganstanford53@gmail.com>
2024-08-14 11:38:56 -0400

    ass

--- admin_handlers/make_vote.py
+++ admin_handlers/make_vote.py
@@ -3,14 +3,16 @@
 async def make_vote_handler(db, message, client):
 
     message_parts = message.content.split('|')
-    if len(message_parts) < 3:
+    if len(message_parts) < 4:
         await message.channel.send('Invalid number of params.')
         return
     
     message_parts.pop(0)
+    title = message_parts.pop(0)
+    upper_title = title.upper()
 
 
-    final_string = 'OPTIONS:'
+    final_string = f'{upper_title}:'
     index = 1
     for message_part in message_parts:
         final_string += '\n'+str(index)+'. '+message_part
