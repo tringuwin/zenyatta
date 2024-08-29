d2b52f81dda5b6e67a4c03b916b023b50a1b2c75
Logan Stanford <loganstanford53@gmail.com>
2024-08-29 19:31:39 -0400

    yo

--- command_handlers/league/schedule.py
+++ command_handlers/league/schedule.py
@@ -60,7 +60,7 @@ async def schedule_handler(db, message, client):
         if weeks == 1:
             break
 
-    final_string += '\n5 Total Weeks in the Regular Season'
+    final_string += '\n\n5 Total Weeks in the Regular Season'
     final_string += '\nSee the full Schedule here: https://spicyragu.netlify.app/sol/schedule'
 
     await message.channel.send(final_string)
