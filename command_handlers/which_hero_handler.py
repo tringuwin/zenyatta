12c9593f1465eacb926eb19d365d736eb2284881
Logan Stanford <loganstanford53@gmail.com>
2024-09-07 12:31:34 -0400

    juno which hero

--- command_handlers/which_hero_handler.py
+++ command_handlers/which_hero_handler.py
@@ -3,7 +3,7 @@ import constants
 
 
 ALL_HEROES = ['Mauga', 'Ana', 'Ashe', 'Baptiste', 'Bastion', 'Brigitte', 'Cassidy', 'D.va', 'Doomfist', 'Echo', 'Genji', 
-              'Hanzo', 'Illari', 'Junker Queen', 'Junkrat', 'Kiriko', 'Lifeweaver', 'Lucio', 'Mauga', 'Mei', 'Mercy', 'Moira', 
+              'Hanzo', 'Illari', 'Junker Queen', 'Junkrat', 'Juno', 'Kiriko', 'Lifeweaver', 'Lucio', 'Mauga', 'Mei', 'Mercy', 'Moira', 
               'Orisa', 'Pharah', 'Ramattra', 'Reaper', 'Reinhardt', 'Roadhog', 'Sigma', 'Sojourn', 'Soldier: 76', 
               'Sombra', 'Symmetra', 'Torbjorn', 'Tracer', 'Venture', 'Widowmaker', 'Winston', 'Wrecking Ball', 'Zarya', 'Zenyatta']
 
