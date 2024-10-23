5b782dd0bbc7654e9b61331c0b5a426701b2bff1
Logan Stanford <loganstanford53@gmail.com>
2024-10-23 18:52:31 -0400

    sus baka

--- common_messages.py
+++ common_messages.py
@@ -1,6 +1,11 @@
 
+import constants
+
 async def not_registered_response(message):
-    await message.channel.send(message.author.mention+" It seems like you're not registered yet. Please go to this channel ( https://discord.com/channels/1130553449491210442/1130553489106411591 ) and use this command to register: **!battle YourBattleTagHere#1234**")
+    if message.channel.id == constants.BOT_CHANNEL:
+        await message.channel.send(message.author.mention+" It seems like you're not registered yet. Please use this command to register: **!battle YourBattleTagHere#1234**")
+    else:
+        await message.channel.send(message.author.mention+" It seems like you're not registered yet. Please go to this channel ( https://discord.com/channels/1130553449491210442/1130553489106411591 ) and use this command to register: **!battle YourBattleTagHere#1234**")
 
 async def invalid_number_of_params(message):
     await message.channel.send("Invalid number of parameters. Please check **!help** for more details.")
