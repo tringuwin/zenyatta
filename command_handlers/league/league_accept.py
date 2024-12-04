1e8489f6dd2c1b7c28f853340fae718d8f65a3f5
Logan Stanford <loganstanford53@gmail.com>
2024-12-03 20:46:29 -0500

    league accept uses emoji embed

--- command_handlers/league/league_accept.py
+++ command_handlers/league/league_accept.py
@@ -131,11 +131,9 @@ async def league_accept_handler(db, message, client):
 
     league_notifs_channel = client.get_channel(constants.TEAM_NOTIFS_CHANNEL)
 
-    guild = await get_guild(client)
-    team_emoji_id = constants.LEAGUE_TO_EMOJI_ID[real_team_name]
-    team_emoji = guild.get_emoji(team_emoji_id)
+    team_emoji_string = constants.TEAM_NAME_TO_EMOJI_EMBED_STRING[real_team_name]
     
-    await league_notifs_channel.send(str(team_emoji)+' User '+message.author.mention+' has joined the team "'+real_team_name+'".')
+    await league_notifs_channel.send(team_emoji_string+' User '+message.author.mention+' has joined the team "'+real_team_name+'".')
     # if div_joined != 0:
     #     await message.channel.send('You have successfully joined this team! You are now locked in **Division '+str(div_joined)+'** for the rest of this season.')
     # else:
