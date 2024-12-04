9cf376ba4013c340abbdb5dafc2f1abfec91462d
Logan Stanford <loganstanford53@gmail.com>
2024-12-03 20:17:12 -0500

    spelling error

--- command_handlers/league/league_kick.py
+++ command_handlers/league/league_kick.py
@@ -19,7 +19,7 @@ async def league_kick_handler(db, message, client):
     is_admin, my_team, team_name, is_owner = await validate_admin(db, message)
     
     if not my_team:
-        await message.channel.send("You're not on a league teame... So you can't kick people... dumbass...")
+        await message.channel.send("You're not on a league team... So you can't kick people... dumbass...")
         return
 
     team_members = my_team['members']
