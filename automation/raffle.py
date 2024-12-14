eb2835a6644fb520fb084d97341e0cc2fd042abe
Logan Stanford <loganstanford53@gmail.com>
2024-12-14 16:57:21 -0500

    set raffle status to active

--- automation/raffle.py
+++ automation/raffle.py
@@ -1,6 +1,6 @@
 import aiohttp
 
-from helpers import get_constant_value
+from helpers import get_constant_value, set_constant_value
 
 async def start_raffle(db, message):
     raffle_status = get_constant_value(db, 'raffle_status')
@@ -12,12 +12,21 @@ async def start_raffle(db, message):
         'platform': 'twitch'
     }
 
+
+    request_error = False
     async with aiohttp.ClientSession() as session:
         async with session.post(
             'https://streamlabs.com/api/v5/giveaway/start/3f166a7a-47c1-42b9-9ae3-de1cf280db5a?token=B032D12F02A4ED3AA822',
             json=raffle_start_payload
         ) as response:
-            start_request = await response.json()
-            print(start_request)
+            if response.status != 200:
+                request_error = True
+                print('failed with code '+str(response.status))
 
+
+    if request_error:
+        await message.channel.send('This command failed. Please let spicy ragu know.')
+        return
+
+    set_constant_value(db, 'raffle_staus', 'ACTIVE') 
     await message.channel.send('500 Token Raffle Started on solnetwork')
