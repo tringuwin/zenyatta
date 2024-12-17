c6781fdc8df19aff53a689421f1b68da8d7c5c93
Logan Stanford <loganstanford53@gmail.com>
2024-12-16 23:09:54 -0500

    test marvel rivals fail

--- command_handlers/league/league_invite.py
+++ command_handlers/league/league_invite.py
@@ -3,7 +3,11 @@ from league import validate_admin
 from user import get_league_invites, get_league_team, user_exists
 
 
-async def league_invite_handler(db, message):
+async def league_invite_handler(db, message, context):
+
+    if context == 'MR':
+        await message.channel.send('Command is not ready yet for Marvel Rivals.')
+        return
 
     valid_admin, _, team_name, _ = await validate_admin(db, message)
 
