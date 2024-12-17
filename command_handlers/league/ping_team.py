58017cb26b6e127ed1b9f11acf69d1db90569b1b
Logan Stanford <loganstanford53@gmail.com>
2024-12-17 11:30:15 -0500

    ping team marvel rivals

--- command_handlers/league/ping_team.py
+++ command_handlers/league/ping_team.py
@@ -1,20 +1,17 @@
 from discord_actions import get_role_by_id
 from league import validate_admin
+from league_helpers import get_league_teams_collection
 
 
 async def ping_team_handler(db, message, client, context):
 
-    if context == 'MR':
-        await message.channel.send('Command is not ready yet for Marvel Rivals.')
-        return
-
-    valid_admin, _, team_name, _ = await validate_admin(db, message)
+    valid_admin, _, team_name, _ = await validate_admin(db, message, context)
 
     if not valid_admin:
         await message.channel.send('You are not an admin of a league team.')
         return
     
-    league_teams = db['leagueteams']
+    league_teams = get_league_teams_collection(db, context)
     team_object = league_teams.find_one({'team_name': team_name})
     team_role_id = team_object['team_role_id']
     team_role = await get_role_by_id(client, team_role_id)
