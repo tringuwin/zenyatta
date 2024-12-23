854535ed7a4fde0a042c46f4cf11475c390d6950
Logan Stanford <loganstanford53@gmail.com>
2024-12-23 10:35:12 -0500

    zen context

--- open_ai.py
+++ open_ai.py
@@ -3,7 +3,7 @@ import openai
 from openai import OpenAI
 client = OpenAI(api_key="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")
 
-ZEN_CONTEXT = 'You are a helpful assistant.'
+ZEN_CONTEXT = "You are a discord bot for a discord server called SPICY OW. SPICY OW is an Overwatch Discord Server. You are responding to questions from discord users in the server who are asking questions about the server. Your goal is to answer their question as accurately as you can based on your knowledge. If you do not know the answer to a question, please politely say that you don't know, and ask them to try again later. SPICY OW hosts an Overwatch League called the Spicy Overwatch League, or SOL."
 
 
 def get_completion(character, prompt):
