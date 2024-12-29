93e488fe17c4fe8cb6974dc75bdb47f14ded89b5
Logan Stanford <loganstanford53@gmail.com>
2024-12-29 14:06:38 -0500

    update score file

---
+++ automation/casting/update_score.py
@@ -0,0 +1,68 @@
+
+
+
+
+
+from helpers import get_constant_value
+
+
+async def update_score(db, message, team_name, score_change):
+
+    team_name_lower = team_name.lower()
+
+    league_season = get_constant_value(db, 'league_season')
+    league_week = get_constant_value(db, 'league_week')
+
+    schedule_db = db['schedule']
+    season = schedule_db.find_one({'season': league_season})
+    schedule_week = season['weeks'][league_week - 1]
+
+    week_days = schedule_week['days']
+    found = False
+    for day in week_days:
+        for match in day['matches']:
+            if match['home'].lower() == team_name_lower:
+                match['home_score'] += score_change
+                found = True
+                break
+            elif match['away'].lower() == team_name_lower:
+                match['away_score'] += score_change
+                found = True
+                break
+
+    if not found:
+        await message.channel.send('Could not find a match this week that includes a team named '+team_name)
+        return
+    
+    schedule_db.update_one({'season': league_season}, {'$set': {'weeks': season['weeks']}})
+    await message.channel.send('Added ')
+
+
+
+
+async def add_point(db, message):
+
+    command_parts = message.content.split()
+    if len(command_parts) != 2:
+        await message.channel.send('Please send the name of the team to add a point to. Example: **!addpoint Polar**')
+        return
+    
+    team_name = command_parts[1]
+
+    await update_score(db, message, team_name, 1)
+
+
+async def remove_point(db, message):
+
+    command_parts = message.content.split()
+    if len(command_parts) != 2:
+        await message.channel.send('Please send the name of the team to add a point to. Example: **!removepoint Polar**')
+        return
+    
+    team_name = command_parts[1]
+
+    await update_score(db, message, team_name, -1)
+
+
+   
+            
