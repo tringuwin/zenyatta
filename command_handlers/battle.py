b3210913c772be4e595dd69c6877c36ec715be1c
Logan Stanford <loganstanford53@gmail.com>
2024-12-31 14:37:48 -0500

    battle tag doesnt start with # validation

--- command_handlers/battle.py
+++ command_handlers/battle.py
@@ -13,6 +13,10 @@ async def battle_link(db, message, client, user, battle_tag):
     if not ('#' in battle_tag):
         await message.channel.send("The Battle Tag you provided seems to be missing the # and numbers at the end. Please include that too.")
         return
+    
+    if battle_tag[0] == '#':
+        await message.channel.send('Battle tags cannot start with the "#" character. Please use this format: BattleTag#1234')
+        return
         
     lower_tag = battle_tag.lower()
     if find_user_with_battle_tag(db, lower_tag):
