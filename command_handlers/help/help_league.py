fb827476e306aa164cf0f1dbe8a15e24a1145af0
Logan Stanford <loganstanford53@gmail.com>
2025-01-01 16:03:36 -0500

    team page in help

--- command_handlers/help/help_league.py
+++ command_handlers/help/help_league.py
@@ -5,6 +5,7 @@ async def help_league_handler(message):
     help_embed = discord.Embed(title='List of league commands:')
     help_embed.add_field(name='!helpleagueadmin', value='See a list of admin commands for league teams.', inline=False)
     help_embed.add_field(name='!schedule', value='See the current league schedule.', inline=False)
+    help_embed.add_field(name='!teampage [team name]', value='Get a link to a webpage for any league team.', inline=False)
     help_embed.add_field(name='!standings', value='See the current league standings.', inline=False)
     help_embed.add_field(name='!fanof [team name]', value='Set which league team you are a fan of!', inline=False)
     help_embed.add_field(name='!rivalof [team name]', value='Set which league team you are a rival of!', inline=False)
