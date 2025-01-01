30b5c146388d7a354f2db5e1cd01d2a226c5290a
Logan Stanford <loganstanford53@gmail.com>
2025-01-01 15:56:27 -0500

    website in help

--- command_handlers/help/help.py
+++ command_handlers/help/help.py
@@ -7,6 +7,7 @@ async def help_handler(message):
     help_embed.add_field(name='!battle BattleTagHere#1234', value='Register your battle tag with the Spicy OW server', inline=False)
     help_embed.add_field(name='!twitch TwitchUsernameHere', value='Add your twitch username to the server so you can be given rewards you earn on stream!', inline=False)
     help_embed.add_field(name='!profile', value='Shows your profile for this Discord Server.', inline=False)
+    help_embed.add_field(name='!website', value="Get a link to our community's official website.'", inline=False)
     help_embed.add_field(name='!helpteams', value='Show a list of commands related to making teams for community tournaments. (This is not the league commands)', inline=False)
     help_embed.add_field(name='!helpcasino', value='Show a list of commands related to the casino channels.', inline=False)
     help_embed.add_field(name='!helpleague', value='Show a list of commands related to the Spicy Overwatch League.', inline=False)
