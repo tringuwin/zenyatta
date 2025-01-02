28bebca1b5a0d2a2842883b6ededcef9e281ec7e
Logan Stanford <loganstanford53@gmail.com>
2025-01-02 14:32:23 -0500

    username checks if you already linked

--- marvel_rivals/base_commands/username/username.py
+++ marvel_rivals/base_commands/username/username.py
@@ -12,24 +12,24 @@ def user_with_username_exists(db, username):
     users = db['users']
     username_user = users.find_one({'rivals_username_lower': username_lower})
 
-    if username_user:
-        return True
-    
-    return False
+    return username_user
 
 
 async def username_handler(db, message):
 
     word_parts = message.content.split()
     rivals_username = make_string_from_word_list(word_parts, 1)
-    print('submitted username "'+str(rivals_username)+'"')
 
     if len(rivals_username) > 30 or len(rivals_username) < 1:
         await message.channel.send('The marvel rivals username you provided is not valid.')
         return
     
-    if user_with_username_exists(db, rivals_username):
-        await message.channel.send('That marvel rivals username is already linked to a user. (Maybe you already linked it?)')
+    user_with_username = user_with_username_exists(db, rivals_username)
+    if user_with_username:
+        if user_with_username['discord_id'] == message.author.id:
+            await message.channel.send('You have already linked this marvel rivals username to your profile.')
+        else:
+            await message.channel.send('That marvel rivals username is already linked to a another discord user. Please contact staff if you need help.')
         return
 
     user_id = message.author.id
