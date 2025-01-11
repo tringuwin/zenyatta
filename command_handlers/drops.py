5d325bc8fe4342bef4d2cff4c32de3e19c3b3434
Logan Stanford <loganstanford53@gmail.com>
2025-01-11 11:49:34 -0500

    check if twitch is linked before showing drops

--- command_handlers/drops.py
+++ command_handlers/drops.py
@@ -1,7 +1,7 @@
 
 
 from common_messages import not_registered_response
-from user import get_user_drop_boxes, user_exists
+from user import get_twitch_username, get_user_drop_boxes, user_exists
 
 
 async def drops(db, message):
@@ -9,6 +9,11 @@ async def drops(db, message):
     user = user_exists(db, message.author.id)
     if not user:
         await not_registered_response(message)
+        return
+    
+    twitch_username = get_twitch_username(user)
+    if twitch_username == '[not set]':
+        await message.channel.send('Your twitch is not linked with this server! Please link it with this command: **!twitch UsernameHere**')
         return
     
     user_drops = get_user_drop_boxes(user)
