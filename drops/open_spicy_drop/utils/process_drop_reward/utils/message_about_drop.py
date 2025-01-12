d4f3b49cbf82899b1b76d922e61d7cf841693113
Logan Stanford <loganstanford53@gmail.com>
2025-01-12 00:41:48 -0500

    spelling fix

--- drops/open_spicy_drop/utils/process_drop_reward/utils/message_about_drop.py
+++ drops/open_spicy_drop/utils/process_drop_reward/utils/message_about_drop.py
@@ -8,4 +8,4 @@ async def message_about_drop(client, user, reward_info):
     guild = await get_guild(client)
     redemptions_channel = guild.get_channel(constants.OFFER_REDEMPTIONS_CHANNEL_ID)
 
-    await redemptions_channel.send(user['battle_tag']+' just got the reward: '+reward_info['message'])
+    await redemptions_channel.send(user['battle_tag']+' just got the reward: '+reward_info['user_message'])
