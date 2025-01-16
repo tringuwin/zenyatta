8e3eefa5b4b787b7f1c212b604bd538d513bf63d
Logan Stanford <loganstanford53@gmail.com>
2025-01-16 14:21:09 -0500

    no picks rn

--- command_handlers/league/picks/picks.py
+++ command_handlers/league/picks/picks.py
@@ -17,10 +17,14 @@ SEASON_TO_PICK_ARRAY = {
 
 
 
-def make_or_fetch_user_picks(picks_db, league_season, user):
+def make_or_fetch_user_picks(picks_db, league_season, user, can_edit_picks):
 
     picks_obj = picks_db.find_one({'season': league_season, 'user_id': user.id})
     if not picks_obj:
+
+        if not can_edit_picks:
+            return None
+
         blank_picks = copy.deepcopy(SEASON_TO_PICK_ARRAY[league_season])
 
         picks_obj = {
@@ -40,14 +44,19 @@ def make_or_fetch_user_picks(picks_db, league_season, user):
 async def picks_handler(db, message):
 
     picks_active = get_constant_value(db, 'picks_active')
-    # if not picks_active:
-    #     await message.channel.send('There are no pick contests available right now. Check back soon!')
-    #     return
+    if not picks_active:
+        await message.channel.send('There are no pick contests available right now. Check back soon!')
+        return
+    
+    can_edit_picks = get_constant_value(db, 'can_edit_picks')
 
     league_season = get_constant_value(db, 'league_season')
     picks_db = db['picks']
 
-    user_picks = make_or_fetch_user_picks(picks_db, league_season, message.author)
+    user_picks = make_or_fetch_user_picks(picks_db, league_season, message.author, can_edit_picks)
+    if not user_picks:
+        await message.channel.send('Picks cannot be edited right now. Make sure to enter the next challenge.')
+        return
 
     picks_message = "Use this link to edit your picks. Don't share this link with anyone, or they'll be able to edit your picks!\n\nhttps://spicyragu.netlify.app/sol/picks/"+user_picks['token']
 
