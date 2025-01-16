a3983d6fc7b84c9c4ba9f49571265ce43cbdcc6c
Logan Stanford <loganstanford53@gmail.com>
2025-01-16 15:50:21 -0500

    score picks

---
+++ admin_handlers/score_picks.py
@@ -0,0 +1,29 @@
+
+
+BEST_POSSIBLE_SCORE = 1
+
+
+
+def calculate_picks_score(picks):
+
+    score = 0
+
+    if picks['round1'][0] == 'Fresas':
+        score += 1
+
+    return score
+
+
+async def score_picks(db, message):
+
+    picks_db = db['picks']
+    all_picks = picks_db.find({'season': 5})
+
+    for pick in all_picks:
+
+        pick_score = calculate_picks_score(pick['picks'])
+        is_perfect = pick_score == BEST_POSSIBLE_SCORE
+
+        picks_db.update_one({'token': pick['token']}, {'$set': {'score': pick_score, 'max': BEST_POSSIBLE_SCORE, 'perfect': is_perfect}})
+
+    await message.channel.send('Command success')
