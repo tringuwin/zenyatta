b2115be958faa690ac52b8104620c5729e848146
Logan Stanford <loganstanford53@gmail.com>
2025-01-21 10:14:25 -0500

    notify fix

--- drops/open_spicy_drop/utils/process_drop_reward/utils/message_about_drop.py
+++ drops/open_spicy_drop/utils/process_drop_reward/utils/message_about_drop.py
@@ -8,6 +8,6 @@ async def message_about_drop(client, user, reward_info):
     guild = await get_guild(client)
     redemptions_channel = guild.get_channel(constants.OFFER_REDEMPTIONS_CHANNEL_ID)
 
-    user_mention = '<@'+user['discord_id']+'>'
+    user_mention = '<@'+str(user['discord_id'])+'>'
 
     await redemptions_channel.send(user_mention+' just got the reward: '+reward_info['user_message'])
