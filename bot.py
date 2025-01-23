c145b1d2a4831cf03246f1e5ed6dcb559b8faabb
Logan Stanford <loganstanford53@gmail.com>
2025-01-23 10:38:46 -0500

    oops

--- bot.py
+++ bot.py
@@ -371,7 +371,7 @@ async def handle_message(message, db, client):
     
     context = 'OW'
     message_channel = message.channel
-    if message_channel.category_id == constants.RIVALS_CATEGORY_ID or message_channel.category_id == constants.RIVALS_TEAM_INFO_CHANNEL:
+    if message_channel.category_id == constants.RIVALS_CATEGORY_ID or message_channel.category_id == constants.RIVALS_TEAMS_CATEGORY_ID:
         context = 'MR'
 
 
