da163c937c1c6936b889ab12339629804011d525
Logan Stanford <loganstanford53@gmail.com>
2025-02-05 17:49:30 -0500

    donate gems

--- command_handlers/help/help_gems.py
+++ command_handlers/help/help_gems.py
@@ -5,6 +5,7 @@ async def help_gems_handler(message):
     help_embed = discord.Embed(title='List of gem commands:')
     help_embed.add_field(name='!gems', value='Show a list of your current gems. Also shows some information about gem values.', inline=False)
     help_embed.add_field(name='!sellgems [color] [amount]', value='Sell gems of any color for 50 Tokens each.', inline=False)
+    help_embed.add_field(name='!donategems @user [color to give] [number]', value='Give gems to another user.', inline=False)
     help_embed.add_field(name='!tradegem [color to give] @Partner [color to get]', value='Make a gem trade offer to another user.', inline=False)
     help_embed.add_field(name='!denygemtrade', value='Deny a gem trade offer.', inline=False)
     help_embed.add_field(name='!acceptgemtrade', value='Accept a gem trade offer.', inline=False)
