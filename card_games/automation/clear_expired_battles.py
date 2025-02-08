2ba536524f8d4201cb7a4f1f96f78c4a6356d237
Logan Stanford <loganstanford53@gmail.com>
2025-02-08 16:54:17 -0500

    remove from battle cards array when battle ends

--- card_games/automation/clear_expired_battles.py
+++ card_games/automation/clear_expired_battles.py
@@ -7,14 +7,24 @@ async def clear_expired_battles(client, db, message):
     card_battles = db['card_battles']
     all_battles = card_battles.find()
 
+    users = db['users']
+
     battles_deleted = 0
 
     current_time = time.time()
     for battle in all_battles:
         if battle['expire_time'] < current_time:
-            card_battles.delete_one({'card_display': battle['card_display']})
+            
+            user = users.find_one({'discord_id': battle['user_id']})
+            user_battle_cards = user['battle_cards']
+            user_battle_cards.remove(battle['card_display'])
+            users.update_one({'discord_id': user['discord_id']}, {'$set': {'battle_cards': user_battle_cards}})
+
             battle_message = await get_message_by_channel_and_id(client, constants.CARD_BATTLE_CHANNEL, battle['message_id'])
             await battle_message.delete()
+
+            card_battles.delete_one({'card_display': battle['card_display']})
+
             battles_deleted += 1
 
     await message.channel.send('Deleted '+str(battles_deleted)+' expired battles.')
