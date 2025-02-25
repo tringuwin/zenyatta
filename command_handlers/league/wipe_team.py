8f6319d524be3dbec31ccce7f5224894ffaf8252
Logan Stanford <loganstanford53@gmail.com>
2025-02-24 21:31:17 -0500

    wipe team final

--- command_handlers/league/wipe_team.py
+++ command_handlers/league/wipe_team.py
@@ -10,6 +10,43 @@ TAKEOVER_USERS = [
 ]
 
 
+async def remove_team_role_from_all_members(message, team, client):
+
+    team_role_id = team['team_role_id']
+    team_role = await get_role_by_id(client, team_role_id)
+    for member in team_role.members:
+
+        if not (member.id in TAKEOVER_USERS):
+            await member.remove_roles(team_role)
+
+    await message.channel.send('Team role removed from all users.')
+
+
+async def clear_members_from_league_team(message, league_teams_collection, team):
+
+    final_members = []
+
+    for member in team['members']:
+        if member['discord_id'] in TAKEOVER_USERS:
+            final_members.append(member)
+
+    league_teams_collection.update_one({'team_name': team['team_name']}, {'$set': {'members': final_members}})
+
+    await message.channel.send('All members removed from team object.')
+
+
+
+async def remove_league_team_from_all_users(message, db, team):
+
+    users = db['users']
+
+    users_with_team = users.find({'league_team': team['team_name']})
+    for user in users_with_team:
+        users.update_one({'discord_id': user['discord_id']}, {'$set': {'league_team': 'None'}})
+
+    await message.channel.send('All users with league team cleared.')
+
+
 async def wipe_team(db, message, client, context):
 
     valid_params, params = valid_number_of_params(message, 2)
@@ -26,11 +63,6 @@ async def wipe_team(db, message, client, context):
         await message.channel.send('There is no team with the name: '+team_name)
         return
     
-    team_role_id = team['team_role_id']
-    team_role = await get_role_by_id(client, team_role_id)
-    for member in team_role.members:
-
-        if not (member.id in TAKEOVER_USERS):
-            await member.remove_roles(team_role)
-
-    await message.channel.send('Team role removed from all users.')
+    await remove_team_role_from_all_members(message, team, client)
+    await clear_members_from_league_team(message, league_teams_collection, team)
+    await remove_league_team_from_all_users(message, db, team)
