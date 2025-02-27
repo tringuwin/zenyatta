09ee8d652d0b4f2ee5aaf4c3f42dc2fd3f8cbb3e
Logan Stanford <loganstanford53@gmail.com>
2025-02-27 18:10:25 -0500

    schedule plan loop logic

--- automation/schedule_plan/schedule_plan_loop/utils/check_for_schedule_start.py
+++ automation/schedule_plan/schedule_plan_loop/utils/check_for_schedule_start.py
@@ -1,5 +1,19 @@
 
 
+from time_helpers import has_date_passed_est
+
+
 async def check_for_schedule_start(schedule_plans, schedule, message):
     
-    pass
+    first_day_of_schedule = schedule['weeks'][0]['days'][0]
+    first_day_date = first_day_of_schedule['date']
+    
+    has_season_started = has_date_passed_est(first_day_date['day'], first_day_date['month'], first_day_date['year'])
+
+    if has_season_started:
+        schedule_plans.update_one({"_id": schedule['_id']}, {"$set": {"status": "IN PROGRESS"}})
+        await message.channel.send(f"Season {schedule['season']} of league {schedule['context']} has started!")
+        return
+
+    await message.channel.send(f"Season {schedule['season']} of league {schedule['context']} has not started yet.")
+    
