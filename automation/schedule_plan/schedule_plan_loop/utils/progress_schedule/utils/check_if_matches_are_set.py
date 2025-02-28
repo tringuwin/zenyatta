8a7ea36c51b17f2be9d1c898493e545f38b64912
Logan Stanford <loganstanford53@gmail.com>
2025-02-28 12:15:23 -0500

    return list of matchups as list

--- automation/schedule_plan/schedule_plan_loop/utils/progress_schedule/utils/check_if_matches_are_set.py
+++ automation/schedule_plan/schedule_plan_loop/utils/progress_schedule/utils/check_if_matches_are_set.py
@@ -8,11 +8,11 @@ def get_all_matchups(db, league_context, season, week):
 
     matchups_collection = db['matchups']
 
-    matchups = matchups_collection.find({
+    matchups = list(matchups_collection.find({
         'context': league_context,
         'season': season,
         'week': week
-    })
+    }))
 
     return matchups
 
