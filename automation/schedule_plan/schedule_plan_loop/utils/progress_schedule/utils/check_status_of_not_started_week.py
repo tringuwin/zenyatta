bf7d06c62bbc68a717c56ab95dcab6cf6ebad41b
Logan Stanford <loganstanford53@gmail.com>
2025-02-28 10:34:39 -0500

    update schedule week

--- automation/schedule_plan/schedule_plan_loop/utils/progress_schedule/utils/check_status_of_not_started_week.py
+++ automation/schedule_plan/schedule_plan_loop/utils/progress_schedule/utils/check_status_of_not_started_week.py
@@ -12,7 +12,7 @@ async def check_status_of_not_started_week(schedule_plans, schedule, week, messa
     has_week_started = has_date_passed_est(first_day_date['day'], first_day_date['month'], first_day_date['year'])
 
     if has_week_started:
-        schedule_plans['weeks'][week_index]['status'] = 'MATCHES'
+        schedule['weeks'][week_index]['status'] = 'MATCHES'
         schedule_plans.update_one({'_id': schedule['_id']}, {'$set': {'weeks': schedule_plans['weeks']}})
         await message.channel.send('Week has started!')
         await notify_staff_for_matches(message, schedule)
