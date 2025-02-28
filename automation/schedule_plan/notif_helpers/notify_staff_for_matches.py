8469b425d2686d4c753190903f107b167b64fd55
Logan Stanford <loganstanford53@gmail.com>
2025-02-28 11:15:22 -0500

    current week

--- automation/schedule_plan/notif_helpers/notify_staff_for_matches.py
+++ automation/schedule_plan/notif_helpers/notify_staff_for_matches.py
@@ -3,5 +3,5 @@ import constants
 
 async def notify_staff_for_matches(message, schedule):
 
-    schedule_message = f' Matches need to be set for league with context {schedule["context"]} for season {schedule["season"]}'
+    schedule_message = f' Matches need to be set for league with context {schedule["context"]} for season {schedule["season"]} week {schedule["current_week"] + 1}'
     await message.channel.send(constants.STAFF_PING+schedule_message)
