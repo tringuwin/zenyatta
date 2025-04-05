75c35f2cfb89637ddecbfd5ab7023372eef81dc6
Logan Stanford <loganstanford53@gmail.com>
2025-04-05 19:49:47 -0400

    buns

--- command_handlers/league/toggle_apps.py
+++ command_handlers/league/toggle_apps.py
@@ -5,7 +5,7 @@ from league import validate_admin
 
 async def toggle_apps_handler(db, message, context):
 
-    valid_admin, _, team_name, _ = await validate_admin(db, message)
+    valid_admin, _, team_name, _ = await validate_admin(db, message, context)
 
     if not valid_admin:
         await message.channel.send('You are not an admin of a league team.')
