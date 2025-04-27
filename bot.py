aeee74b73a3137dbc92b5a2e2504ac8776d9bfd9
Logan Stanford <loganstanford53@gmail.com>
2025-04-27 00:45:43 -0400

    remove passes command

--- bot.py
+++ bot.py
@@ -1033,6 +1033,24 @@ async def handle_message(message, db, client):
         # !addevent|[event id]|[event name]|[max participants]|[0 for no pass, 1 for pass, 2 for subs]|[team size]|[event role id]|[event channel id]
         await add_event_handler(db, message)
 
+    elif lower_message.startswith("!removepasses") and is_admin:
+
+        users = db['users']
+        all_users = users.find()
+        users_affected = 0
+        for user in all_users:
+            if 'passes' in user and 'tokens' in user:
+                num_passes = user['passes']
+                if num_passes > 0:
+                    tokens_to_give = num_passes * 10
+                    new_tokens = user['tokens'] + tokens_to_give
+                    users.update_one({'discord_id': user['discord_id']}, {'$set': {'tokens': new_tokens, 'passes': 0}})
+                    users_affected += 1
+
+        await message.channel.send(f'All users have had their passes removed and received 10 tokens for each pass they had. {users_affected} users were affected.')
+        
+
+
     elif lower_message.startswith("!delevent") and is_admin:
         # !delevent [event id]
         await delete_event_handler(db, message)
