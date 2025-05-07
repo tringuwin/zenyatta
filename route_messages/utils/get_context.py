049176039883f5b8452c2e5f1d607277156791e9
Logan Stanford <loganstanford53@gmail.com>
2025-05-07 14:27:29 -0400

    rivals admin channel in staff section

--- route_messages/utils/get_context.py
+++ route_messages/utils/get_context.py
@@ -5,7 +5,7 @@ def get_context(message):
     context = 'OW'
     message_channel = message.channel
 
-    if message_channel.category_id == constants.RIVALS_CATEGORY_ID or message_channel.category_id == constants.RIVALS_TEAMS_CATEGORY_ID or message_channel.id == constants.MARVEL_RIVALS_MATCH_COMMANDS_CHANNEL:
+    if message_channel.category_id == constants.RIVALS_CATEGORY_ID or message_channel.category_id == constants.RIVALS_TEAMS_CATEGORY_ID or message_channel.id == constants.MARVEL_RIVALS_MATCH_COMMANDS_CHANNEL or message.channel.id == constants.MARVEL_ADMIN_COMMAND_CHANNEL:
         context = 'MR'
 
     elif message_channel.category_id == constants.VALORANT_CATEGORY_ID or message_channel.category_id == constants.VALORANT_TEAMS_CATEGORY_ID:
