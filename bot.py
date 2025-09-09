0e5789b44f505423b0f65b9180f7059f3810c298
Logan Stanford <loganstanford53@gmail.com>
2025-09-09 00:51:20 -0400

    coinstats command

--- bot.py
+++ bot.py
@@ -61,6 +61,7 @@ from casting.delete_caster import delete_caster_handler
 from casting.make_caster import make_caster_handler
 from casting.make_lobby_admin import make_lobby_admin_handler
 from casting.pay import pay_handler
+from coin.coin_stats import coin_stats
 from command_handlers.accept_gem_trade import accept_gem_trade_handler
 from command_handlers.auction.bid import bid_handler
 from command_handlers.auction.end_auction import end_auction_handler
@@ -1828,6 +1829,10 @@ async def handle_message(message, db, client):
             'test': 1
         }
         test2 = test['test2']
+
+
+    elif lower_message == '!coinstats':
+        await coin_stats(db, message)
 
     elif lower_message == '!forcescheduleloop' and is_admin:
 
---
+++ coin/coin_stats.py
@@ -0,0 +1,34 @@
+
+
+from unittest import result
+from constants import SPICY_COIN_EMOJI_STRING
+from helpers import get_constant_value
+
+TOTAL_COINS = 10_000_000 # 10 million coins in total
+
+
+def percent_of_coins(amount):
+
+    raw_percent = (amount / TOTAL_COINS * 100) if TOTAL_COINS > 0 else 0
+    return round(raw_percent, 2)
+
+
+async def coin_stats(db, message):
+
+    free_vouchers = get_constant_value(db, 'free_vouchers')
+
+    users = db['users']
+
+    result = users.aggregate([
+        {"$group": {"_id": None, "total_vouchers": {"$sum": "$vouchers"}}}
+    ])
+
+    owned_vouchers = next(result, {}).get("total_vouchers", 0)
+
+    coins_in_market = TOTAL_COINS - free_vouchers - owned_vouchers
+
+    coin_message = f'{SPICY_COIN_EMOJI_STRING} **Spicy Coin Stats** {SPICY_COIN_EMOJI_STRING}'
+    coin_message += f'\n\n**Total Spicy Coins:** {TOTAL_COINS:,}'
+    coin_message += f'\n**Free Vouchers:** {free_vouchers:,} ({percent_of_coins(free_vouchers)}%)'
+    coin_message += f'\n**Owned Vouchers:** {owned_vouchers:,} ({percent_of_coins(owned_vouchers)}%)'
+    coin_message += f'\n**Coins in Market:** {coins_in_market:,} ({percent_of_coins(coins_in_market)}%)'
