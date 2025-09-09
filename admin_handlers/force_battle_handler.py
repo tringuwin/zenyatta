70b062f4f7929e29d676a5df425aa5a4cd3e5f33
Tri nguyen <tringuyen@Tris-MacBook-Pro.local>
2025-09-09 00:25:16 -0700

    Quick fixes for @everyone and @here

--- admin_handlers/force_battle_handler.py
+++ admin_handlers/force_battle_handler.py
@@ -7,6 +7,9 @@ async def force_battle_handler(db, message, client):
     
     parts = message.content.split()
     battle_tag = parts[2]
+    if battle_tag.lower == "@everyone" or "@here":
+        await message.channel.send('User not found.')
+        return
     user = message.mentions[0]
 
     await battle_link(db, message, client, user, battle_tag)
