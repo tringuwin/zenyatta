e5bf2c20dae5f8836078b0ad74bd7a89343fb20a
Logan Stanford <loganstanford53@gmail.com>
2025-09-10 15:33:56 -0400

    sol page default in website

--- command_handlers/website.py
+++ command_handlers/website.py
@@ -4,4 +4,4 @@ from safe_send import safe_send
 
 async def website_handler(message):
 
-    await safe_send(message.channel, f'Check out the official SOL website here!\n\n{constants.WEBSITE_DOMAIN}')
+    await safe_send(message.channel, f'Check out the official SOL website here!\n\n{constants.WEBSITE_DOMAIN}/sol')
