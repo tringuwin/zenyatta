32354cb0300d70d4f0a54688b80f9128b38eed13
Tri Nguyen <Trihnguyen2005@gmail.com>
2025-09-24 11:38:11 -0700

    added bot validity

--- bot.py
+++ bot.py
@@ -297,43 +297,7 @@ from xp_battles import add_to_battle, how_many_handler, remove_from_battle
 from wipe_bracket import wipe_bracket_handler
 from switch_matches import switch_matches_handler
 from gen_tourney import gen_tourney_handler
-
-def is_valid_channel(message, lower_message, is_helper, is_push_bot, is_tourney_admin):
-
-    if is_helper or is_push_bot or is_tourney_admin:
-        return True, None
-
-    # Wager Command
-    if lower_message.startswith('!wager'):
-        if message.channel.id != constants.ROULETTE_CHANNEL:
-            return False, 'Please only use the wager command in the Roulette Channel.'
-
-    # Blackjack Command
-    elif lower_message.startswith('!blackjack'):
-        if message.channel.id != constants.BLACKJACK_CHANNEL:
-            return False, 'Please only use the blackjack command in the Blackjack Channel.'
-
-    # Mine Command
-    elif lower_message.startswith('!mine'):
-        if message.channel.id != constants.MINE_CHANNEL:
-            return False, 'Please only use the mine command in the Mineshaft Channel.'
-
-    # RPS Command
-    elif lower_message.startswith('!rps'):
-        if message.channel.id != constants.RPS_CHANNEL:
-            return False, 'Please only use the rps command in the RPS Channel.'
-
-    # Open Pack Command
-    elif lower_message.startswith('!openpack'):
-        if message.channel.id != constants.PACK_OPEN_CHANNEL:
-            return False, 'Please only open packs in the packs opening channel: https://discord.com/channels/1130553449491210442/1233596350306713600'
-
-    # Open Drop Command
-    elif lower_message.startswith('!opendrop'):
-        if message.channel.id != constants.OPENING_DROPS_CHANNEL:
-            return False, 'Please only open drops in the drops opening channel: https://discord.com/channels/1130553449491210442/1332055598057001021'
-    
-    return True, None
+from bot_validity import is_valid_channel
 
 
 
